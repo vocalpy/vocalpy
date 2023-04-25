228e83f045e7135fb64f93775902bfc3fb0a41c1
David Nicholson <nickledave@users.noreply.github.com>
2023-04-25 08:34:06 -0600

    Move dataclasses/Audio + Spectrogram -> domain_model

--- src/vocalpy/domain_model/__init__.py
+++ src/vocalpy/domain_model/__init__.py
@@ -6,22 +6,26 @@ The model is meant to allow for
 decoupling the dataset itself from the means of persistent storage,
 e.g. a database.
 """
+from .audio import Audio
 from .annotation_file import AnnotationFile
 from .audio_file import AudioFile
 from .dataset import Dataset
 from .dataset_file import DatasetFile, DatasetFileType, DatasetFileTypeEnum
 from .feature_file import FeatureFile
+from .spectrogram import Spectrogram
 from .spectrogram_file import SpectrogramFile
 from .spectrogram_parameters import SpectrogramParameters
 
 __all__ = [
     'AnnotationFile',
+    'Audio',
     'AudioFile',
     'Dataset',
     'DatasetFile',
     'DatasetFileType',
     'DatasetFileTypeEnum',
     'FeatureFile',
+    'Spectrogram',
     'SpectrogramFile',
     'SpectrogramParameters',
 ]
