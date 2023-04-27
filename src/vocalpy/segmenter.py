e207d7d4d4071219dee370f11371c203841f29f7
David Nicholson <nickledave@users.noreply.github.com>
2023-04-26 22:20:36 -0400

    WIP: Rewrite segmenter

---
+++ src/vocalpy/segmenter.py
@@ -0,0 +1,14 @@
+from typing import Callable
+
+from .domain_model import Dataset, Sequence
+
+
+class Segmenter:
+    def __init__(self, callback: Callable, params: dict):
+        self.callback = callback
+        self.params = params
+
+    def segment(self, dataset: Dataset) -> list[Sequence]:
+        # TODO: figure out how to handle case where we segment with features
+        # how will Segmenter know whether to use spectrogram, audio, etc.?
+        pass
