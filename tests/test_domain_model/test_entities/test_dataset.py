4ca9d0b4acd01717f1d2c041de6f6f09ec725b8a
David Nicholson <nickledave@users.noreply.github.com>
2023-04-30 18:34:55 -0400

    Fix unit tests in test_entities/test_dataset.py

--- tests/test_domain_model/test_entities/test_dataset.py
+++ tests/test_domain_model/test_entities/test_dataset.py
@@ -6,6 +6,7 @@ import vocalpy
 class TestDataset:
 
     def test_audio_and_spect_none_raises(self):
+        # TODO: rewrite to test that not all DatasetFiles raises
         """Test that __attrs_post_init__ raises an error when both audio_files and spect_files are None (default)"""
         with pytest.raises(ValueError):
             vocalpy.domain_model.entities.Dataset()
@@ -23,20 +24,27 @@ class TestDataset:
             for annot_path in annot_paths
         ]
 
+        files = []
+        for file in audio_files + annot_files:
+            files.append(
+                vocalpy.domain_model.entities.DatasetFile(
+                    file=file,
+                    file_type=vocalpy.domain_model.entities.DatasetFileTypeEnum[file.__class__.__name]
+                )
+            )
+
         dataset = vocalpy.Dataset(
-            audio_files=audio_files,
-            annotation_files=annot_files,
+            files=files
         )
 
         assert isinstance(dataset, vocalpy.Dataset)
-        assert dataset.audio_files == audio_files
-        assert dataset.annotation_files == annot_files
+        assert dataset.files == files
 
     def test_init_with_spect_mat_annot_yarden_root(self, spect_mat_annot_yarden_root):
-        audio_paths = vocalpy.paths.from_dir(spect_mat_annot_yarden_root, 'cbin')
-        audio_files = [
-            vocalpy.domain_model.entities.AudioFile(path=audio_path)
-            for audio_path in audio_paths
+        spect_paths = vocalpy.paths.from_dir(spect_mat_annot_yarden_root, '.spect.mat')
+        spect_files = [
+            vocalpy.domain_model.entities.SpectrogramFile(path=spect_path)
+            for spect_path in spect_paths
         ]
 
         annot_paths = vocalpy.paths.from_dir(spect_mat_annot_yarden_root, '.not.mat')
@@ -45,11 +53,18 @@ class TestDataset:
             for annot_path in annot_paths
         ]
 
+        files = []
+        for file in spect_files + annot_files:
+            files.append(
+                vocalpy.domain_model.entities.DatasetFile(
+                    file=file,
+                    file_type=vocalpy.domain_model.entities.DatasetFileTypeEnum[file.__class__.__name]
+                )
+            )
+
         dataset = vocalpy.Dataset(
-            audio_files=audio_files,
-            annotation_files=annot_files,
+            files=files
         )
 
         assert isinstance(dataset, vocalpy.Dataset)
-        assert dataset.audio_files == audio_files
-        assert dataset.annotation_files == annot_files
+        assert dataset.files == files
