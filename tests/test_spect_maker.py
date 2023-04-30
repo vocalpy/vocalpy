14837b312fd78dd1f258f3bbc2ec5110777512d1
David Nicholson <nickledave@users.noreply.github.com>
2023-04-30 18:36:54 -0400

    Run pre-commit on tests/

--- tests/test_spect_maker.py
+++ tests/test_spect_maker.py
@@ -4,16 +4,15 @@ import vocalpy
 
 
 @pytest.mark.parametrize(
-    'audio_format',
+    "audio_format",
     [
-        'wav',
-        'cbin',
-    ]
+        "wav",
+        "cbin",
+    ],
 )
-def test_default_spect_fname_func(specific_audio_dir,
-                                  audio_format):
+def test_default_spect_fname_func(specific_audio_dir, audio_format):
     audio_dir = specific_audio_dir(audio_format)
-    audio_paths = sorted(audio_dir.glob(f'*{audio_format}'))
+    audio_paths = sorted(audio_dir.glob(f"*{audio_format}"))
 
     for audio_path in audio_paths:
         spect_path = vocalpy.spect_maker.default_spect_fname_func(audio_path)
@@ -21,21 +20,18 @@ def test_default_spect_fname_func(specific_audio_dir,
 
 
 @pytest.mark.parametrize(
-    'audio_format, output_dir',
+    "audio_format, output_dir",
     [
-        ('wav', None),
-        ('cbin', 'tmp_path'),
-    ]
+        ("wav", None),
+        ("cbin", "tmp_path"),
+    ],
 )
-def test_spect_maker_audio_paths(specific_audio_dir,
-                                 audio_format,
-                                 output_dir,
-                                 tmp_path):
+def test_spect_maker_audio_paths(specific_audio_dir, audio_format, output_dir, tmp_path):
     audio_dir = specific_audio_dir(audio_format)
-    if output_dir == 'tmp_path':
+    if output_dir == "tmp_path":
         output_dir = tmp_path
 
-    audio_paths = sorted(audio_dir.glob(f'*{audio_format}'))
+    audio_paths = sorted(audio_dir.glob(f"*{audio_format}"))
     spect_maker = vocalpy.SpectMaker
     spects = spect_maker.make(audio_paths, output_dir=output_dir)
     assert all([isinstance(spect, vocalpy.Spectrogram) for spect in spects])
