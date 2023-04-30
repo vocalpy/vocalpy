a3f24a225951645db8b851391b3c76b614b4b714
David Nicholson <nickledave@users.noreply.github.com>
2023-04-30 15:08:49 -0400

    Fix absolute module references in test_entities/test_annotation_file.py

--- tests/test_domain_model/test_entities/test_annotation_file.py
+++ tests/test_domain_model/test_entities/test_annotation_file.py
@@ -12,15 +12,15 @@ class TestAnnotationFile:
         for the case where one annotation file
         annotates one audio file"""
         audio_path, annot_path = a_cbin_notmat_pair
-        audio_file = vocalpy.dataset.AudioFile(path=audio_path)
-        annot_file = vocalpy.dataset.AnnotationFile(
+        audio_file = vocalpy.domain_model.entities.AudioFile(path=audio_path)
+        annot_file = vocalpy.domain_model.entities.AnnotationFile(
             path=annot_path,
             annotates=audio_file
         )
-        assert isinstance(annot_file, vocalpy.dataset.AnnotationFile)
+        assert isinstance(annot_file, vocalpy.domain_model.entities.AnnotationFile)
         for attr_name, attr_type, attr_val in zip(
                 ('path', 'annotates'),
-                (pathlib.Path, vocalpy.dataset.AudioFile),
+                (pathlib.Path, vocalpy.domain_model.entities.AudioFile),
                 (annot_path, audio_file)
         ):
             assert hasattr(annot_file, attr_name)
@@ -29,14 +29,14 @@ class TestAnnotationFile:
 
     def test_init_list_of_wav(self, annot_file_koumura, audio_list_wav):
         audio_files = [
-            vocalpy.dataset.AudioFile(path=wav_path)
+            vocalpy.domain_model.entities.AudioFile(path=wav_path)
             for wav_path in audio_list_wav
         ]
-        annot_file = vocalpy.dataset.AnnotationFile(
+        annot_file = vocalpy.domain_model.entities.AnnotationFile(
             path=annot_file_koumura,
             annotates=audio_files
         )
-        assert isinstance(annot_file, vocalpy.dataset.AnnotationFile)
+        assert isinstance(annot_file, vocalpy.domain_model.entities.AnnotationFile)
         for attr_name, attr_type, attr_val in zip(
                 ('path', 'annotates'),
                 (pathlib.Path, list),
@@ -49,14 +49,14 @@ class TestAnnotationFile:
     def test_init_list_of_spect(self, annot_file_yarden, spect_list_mat):
         # TODO: deal with the fact we have .mat files here
         spect_files = [
-            vocalpy.dataset.SpectrogramFile(path=mat_spect_path)
+            vocalpy.domain_model.entities.SpectrogramFile(path=mat_spect_path)
             for mat_spect_path in spect_list_mat
         ]
-        annot_file = vocalpy.dataset.AnnotationFile(
+        annot_file = vocalpy.domain_model.entities.AnnotationFile(
             path=annot_file_yarden,
             annotates=spect_files
         )
-        assert isinstance(annot_file, vocalpy.dataset.AnnotationFile)
+        assert isinstance(annot_file, vocalpy.domain_model.entities.AnnotationFile)
         for attr_name, attr_type, attr_val in zip(
                 ('path', 'annotates'),
                 (pathlib.Path, list),
@@ -68,17 +68,17 @@ class TestAnnotationFile:
 
     def test_raises(self, annot_file_koumura, audio_list_wav, spect_list_mat):
         audio_files = [
-            vocalpy.dataset.AudioFile(path=wav_path)
+            vocalpy.domain_model.entities.AudioFile(path=wav_path)
             for wav_path in audio_list_wav
         ]
         spect_files = [
-            vocalpy.dataset.SpectrogramFile(path=mat_spect_path)
+            vocalpy.domain_model.entities.SpectrogramFile(path=mat_spect_path)
             for mat_spect_path in spect_list_mat
         ]
         mixed_list = audio_files + spect_files
 
         with pytest.raises(TypeError):
-            annot_file = vocalpy.dataset.AnnotationFile(
+            vocalpy.domain_model.entities.AnnotationFile(
                 path=annot_file_koumura,
                 annotates=mixed_list
             )
