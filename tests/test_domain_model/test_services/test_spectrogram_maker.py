4b3c399d2499144531c0ac5efa2222416f0d8313
David Nicholson <nickledave@users.noreply.github.com>
2023-04-30 19:27:00 -0400

    fixup add test_services, move module

--- tests/test_domain_model/test_services/test_spectrogram_maker.py
+++ tests/test_domain_model/test_services/test_spectrogram_maker.py
@@ -15,7 +15,7 @@ def test_default_spect_fname_func(specific_audio_dir, audio_format):
     audio_paths = sorted(audio_dir.glob(f"*{audio_format}"))
 
     for audio_path in audio_paths:
-        spect_path = vocalpy.spect_maker.default_spect_fname_func(audio_path)
+        spect_path = vocalpy.domain_model.services.spectrogram_maker.default_spect_fname_func(audio_path)
         assert spect_path == audio_path.parent / (audio_path.name + vocalpy.constants.SPECT_FILE_EXT)
 
 
@@ -32,7 +32,7 @@ def test_spect_maker_audio_paths(specific_audio_dir, audio_format, output_dir, t
         output_dir = tmp_path
 
     audio_paths = sorted(audio_dir.glob(f"*{audio_format}"))
-    spect_maker = vocalpy.SpectMaker
+    spect_maker = vocalpy.SpectrogramMaker
     spects = spect_maker.make(audio_paths, output_dir=output_dir)
     assert all([isinstance(spect, vocalpy.Spectrogram) for spect in spects])
     if output_dir is not None:
