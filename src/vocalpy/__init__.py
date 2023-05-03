65ee4ab9a539cfd1c4fd99497a1648b8816c5ce6
David Nicholson <nickledave@users.noreply.github.com>
2023-05-03 19:54:53 -0400

    Fix imports in vocalpy/__init__.py after moving modules

--- src/vocalpy/__init__.py
+++ src/vocalpy/__init__.py
@@ -1,21 +1,18 @@
 from . import constants, dataset, paths, signal, validators
-from .domain_model import (
-    AnnotationFile,
-    Audio,
-    AudioFile,
-    Dataset,
-    DatasetFile,
-    DatasetFileType,
-    DatasetFileTypeEnum,
-    FeatureFile,
-    Segmenter,
-    Sequence,
-    Spectrogram,
-    SpectrogramFile,
-    SpectrogramMaker,
-    SpectrogramParameters,
-    Unit,
-)
+from .annotation_file import AnnotationFile
+from .audio import Audio
+from .audio_file import AudioFile
+from .dataset import Dataset
+from .dataset_file import DatasetFile, DatasetFileType, DatasetFileTypeEnum
+from .feature_file import FeatureFile
+from .segmenter import Segmenter
+from .sequence import Sequence
+from .spectrogram import Spectrogram
+from .spectrogram_file import SpectrogramFile
+from .spectrogram_maker import SpectrogramMaker
+from .spectrogram_parameters import SpectrogramParameters
+from .unit import Unit
+
 
 __all__ = [
     "AnnotationFile",
