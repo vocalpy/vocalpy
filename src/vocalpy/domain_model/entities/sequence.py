d55d65bcf1fc6430843cebded694beeff9a0425a
David Nicholson <nickledave@users.noreply.github.com>
2023-05-03 19:40:56 -0400

    Fix units validator for Sequence

--- src/vocalpy/domain_model/entities/sequence.py
+++ src/vocalpy/domain_model/entities/sequence.py
@@ -12,7 +12,7 @@ class Sequence:
     units: list[Unit] = attrs.field()
     @units.validator
     def is_list_of_unit(self, attribute, value):
-        if not isinstance(list, value) or not all([isinstance(item, Unit) for item in value]):
+        if not isinstance(value, list) or not all([isinstance(item, Unit) for item in value]):
             raise ValueError(
                 f"`units` must be a list of vocalpy.Unit instances"
             )
