f66535c76ef107a339b96a844424eb51b5fc384e
David Nicholson <nickledave@users.noreply.github.com>
2023-05-03 18:37:25 -0400

    Add `source_audio_path` attribute to Sequence

--- src/vocalpy/domain_model/entities/sequence.py
+++ src/vocalpy/domain_model/entities/sequence.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import pathlib
+
 import attrs
 
 from .unit import Unit
@@ -8,10 +10,16 @@ from .unit import Unit
 @attrs.define
 class Sequence:
     units: list[Unit] = attrs.field()
-
     @units.validator
     def is_list_of_unit(self, attribute, value):
         if not isinstance(list, value) or not all([isinstance(item, Unit) for item in value]):
             raise ValueError(
                 f"`units` must be a list of vocalpy.Unit instances"
             )
+
+    source_audio_path : pathlib.Path = attrs.field(converter=attrs.converters.optional(pathlib.Path),
+                                                   validator=attrs.validators.optional(
+                                                       attrs.validators.instance_of(pathlib.Path)
+                                                   ),
+                                                   default=None)
+
