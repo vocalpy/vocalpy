d2856a69cad861e2b632ae713624e6a051ddfd37
David Nicholson <nickledave@users.noreply.github.com>
2023-05-07 17:43:33 -0400

    Add src/vocalpy/__about__.py

---
+++ src/vocalpy/__about__.py
@@ -0,0 +1,38 @@
+import os.path
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__commit__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+
+try:
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+except NameError:
+    base_dir = None
+
+
+__title__ = "vocalpy"
+__summary__ = "A core package for acoustic communication research in Python"
+__uri__ = "https://github.com/vocalpy/vocalpy"
+
+__version__ = "0.1.0"
+
+if base_dir is not None and os.path.exists(os.path.join(base_dir, ".commit")):
+    with open(os.path.join(base_dir, ".commit")) as fp:
+        __commit__ = fp.read().strip()
+else:
+    __commit__ = None
+
+__author__ = "David Nicholson"
+__email__ = "nicholdav@gmail.com"
+
+__license__ = "BSD"
+__copyright__ = "2022-present %s" % __author__
