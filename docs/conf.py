5a262f820401101f693ebcc569a994d5f3368975
David Nicholson <nickledave@users.noreply.github.com>
2023-05-07 22:21:23 -0400

    Add initial docs files from sphinx-quickstart

---
+++ docs/conf.py
@@ -0,0 +1,101 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'VocalPy'
+copyright = '2023, David Nicholson'
+author = 'David Nicholson'
+release = ''
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_nb',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.coverage',
+    'sphinx_copybutton',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode',
+    'sphinx_design',
+    'sphinxext.opengraph',
+    'sphinx_tabs.tabs',
+]
+
+templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+source_suffix = ['.rst', '.md']
+
+# The master toctree document.
+master_doc = 'index'
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = None
+
+myst_enable_extensions = [
+    "dollarmath",
+    # "amsmath",
+    # "deflist",
+    # "html_admonition",
+    # "html_image",
+    "colon_fence",
+    # "smartquotes",
+    # "replacements",
+    # "linkify",
+    # "substitution",
+]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_book_theme'
+
+html_logo = "_static/crowsetta-primary-logo.png"
+
+html_theme_options = {
+    "logo_only": True,
+    "show_toc_level": 1,
+}
+
+html_static_path = ['_static']
+
+# -- Extension configuration -------------------------------------------------
+autosummary_generate = True
+
+# -- Options for intersphinx extension ---------------------------------------
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+}
+
+# -- Options for nitpicky mode
+
+# ensure that all references in the docs resolve.
+nitpicky = True
+nitpick_ignore = []
+
+for line in open('nitpick-ignore.txt'):
+    if line.strip() == "" or line.startswith("#"):
+        continue
+    dtype, target = line.split(None, 1)
+    target = target.strip()
+    nitpick_ignore.append((dtype, target))
---
+++ docs/index.md
@@ -0,0 +1,83 @@
+---
+jupytext:
+  text_representation:
+    extension: .md
+    format_name: myst
+    format_version: 0.13
+    jupytext_version: 1.14.5
+kernelspec:
+  display_name: Python 3 (ipykernel)
+  language: python
+  name: python3
+---
+
+::::{grid}
+:reverse:
+:gutter: 2 1 1 1
+:margin: 4 4 1 1
+
+:::{grid-item}
+:columns: 8
+:class: sd-fs-3
+
+A core package for acoustic communication research in Python.
+:::
+
+:::{grid-item}
+:columns: 4
+
+```{image} ./_static/vocalpy-secondary.png
+:width: 150px
+```
+:::
+
+::::
+
+# Getting Started
+
+If you are new to the library, start with {ref}`tutorial`.
+
+```{toctree}
+:hidden: true
+:maxdepth: 2
+
+tutorial
+howto
+api/index
+development/index
+```
+
+# Support
+
+To report a bug or request a feature (such as a new annotation format), 
+please use the issue tracker on GitHub:  
+<https://github.com/vocalpy/vocalpy/issues>
+
+To ask a question about vocalpy, discuss its development, 
+or share how you are using it, 
+please start a new topic on the VocalPy forum 
+with the vocalpy tag:  
+<https://forum.vocalpy.org/>
+
+# Contribute
+
+- Issue Tracker: <https://github.com/vocalpy/vocalpy/issues>
+- Source Code: <https://github.com/vocalpy/vocalpy>
+
+# License
+
+The project is licensed under the
+[BSD license](https://github.com/vocalpy/vocalpy/blob/master/LICENSE).
+
+# CHANGELOG
+
+You can see project history and work in progress in the
+[CHANGELOG](https://github.com/vocalpy/vocalpy/blob/main/doc/CHANGELOG.md).
+
+# Citation
+
+If you use vocalpy, please cite the DOI:
+
+```{image} https://zenodo.org/badge/DOI/10.5281/zenodo.7905426.svg
+:target: https://doi.org/10.5281/zenodo.7905426
+```
