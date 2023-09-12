5ea3b4953966f4a5c7684d18a280fbc135aa370d
David Nicholson <nickledave@users.noreply.github.com>
2023-09-12 14:53:15 -0400

    Add module-level docstring in src/vocalpy/segment/__init__.py

--- src/vocalpy/segment/__init__.py
+++ src/vocalpy/segment/__init__.py
@@ -1,3 +1,12 @@
+"""Algorithms for segmentation.
+
+These include algorithms for segmenting audio
+and/or spectrograms into line segments
+with start and stop times,
+and algorithms that find bounding boxes in spectrograms
+with high and low frequencies in addition
+to the start and stop times.
+"""
 from .audio_amplitude import audio_amplitude
 # from .spectral_power import
 
