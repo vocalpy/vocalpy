da41bd883473918a9d65d8300b87157427f5f190
David Nicholson <nickledave@users.noreply.github.com>
2023-09-12 18:08:14 -0400

    Change vocalpy/segment/ava_segmenter.py to preserve behavior of original function from ava; remove changes I made

--- src/vocalpy/segment/ava_segmenter.py
+++ src/vocalpy/segment/ava_segmenter.py
@@ -12,8 +12,9 @@ EPSILON = 1e-9
 
 
 def spectrogram_ava(data: npt.NDArray, samplerate: int, nperseg: int, noverlap: int,
-             min_freq: int, max_freq: int) -> tuple[npt.NDArray, npt.NDArray, npt.NDArray]:
-    """Generate a spectrogram similar to how AVA does it.
+                    min_freq: int, max_freq: int, spect_min_val: float, spect_max_val: float
+                    ) -> tuple[npt.NDArray, npt.NDArray, npt.NDArray]:
+    """Compute a spectrogram the same way the ``ava`` library does.
 
     Parameters
     ----------
@@ -29,10 +30,12 @@ def spectrogram_ava(data: npt.NDArray, samplerate: int, nperseg: int, noverlap: 
         Minimum frequency.
     max_freq : int
         Maximum frequency.
+    spect_min_val : float
+    spect_max_val : float
 
     Returns
     -------
-    spec : numpy.ndarray
+    spect : numpy.ndarray
         Spectrogram, a matrix with shape ``(f, t)``.
     f : numpy.ndarray
         Vector of frequencies, same size as
@@ -51,9 +54,12 @@ def spectrogram_ava(data: npt.NDArray, samplerate: int, nperseg: int, noverlap: 
     max_freq_ind = np.searchsorted(f, max_freq)
     f, spect = f[min_freq_ind:max_freq_ind], spect[min_freq_ind:max_freq_ind]
     spect = np.log(np.abs(spect) + EPSILON)
-    # min-max scaling between 0. and 1.
-    spect_min, spect_max = spect.min(), spect.max()
-    spect = (spect - spect_min) / (spect_max - spect_min)
+    # This is like min-max scaling between 0. and 1,
+    # but notice that we could end up with all values set to zero
+    # depending on range of spectrogram before transformation.
+    # An alternative would be to do ``spect = (spect - spect.min()) / (spect.max() - spect.min())``.
+    spect = (spect - spect_min_val) / (spect_max_val - spect_min_val)
+    spect = np.clip(spect, 0., 1.)
     return spect, f, t
 
 
@@ -64,7 +70,7 @@ def softmax(arr: npt.NDArray, t=0.5):
     return np.sum(np.multiply(arr, temp), axis=0)
 
 
-def ava_like(
+def ava_segmenter(
     data: npt.NDArray, samplerate: int,
     spect_callback: Callable | None = None,
     th_1: float = 0.1, th_2: float = 0.2, th_3: float = 0.3,
@@ -72,8 +78,8 @@ def ava_like(
     use_softmax_amp: bool = True, temperature: float = 0.5,
     smoothing_timescale: float = 0.007,
 ):
-    """Find segments in audio, using an algorithm
-    very similar to the one used by the ``ava`` package.
+    """Find segments in audio, using algorithm
+    from ``ava`` package.
 
     Segments audio by generating a spectrogram from it,
     summing power across frequencies, and then thresholding
@@ -98,7 +104,11 @@ def ava_like(
     audio : numpy.ndarray
         Raw audio samples.
     samplerate : int
-        Sampling rate for audio
+        Sampling rate for audio.
+    spect_callback : callable, optional
+        Function used to compute spectrogram.
+        If None, then :func:`vocalpy.segment.ava_segmenter.ava_spectrogram`
+        is used with the default parameters.
     thresh_max : float
         Threshold used to find local maxima.
     thresh_min : float
@@ -139,6 +149,9 @@ def ava_like(
 
     Code is adapted from [2]_.
 
+    Versions of this algorithm were also used to segment 
+    rodent vocalizations in [4]_ and [5]_.
+
     References
     ----------
     .. [1] Goffinet
@@ -146,17 +159,16 @@ def ava_like(
     .. [2] Code
 
     .. [3] Dataset
+    
+    .. [4] Jourjine
+    
+    .. [5] Peterson
     """
     if spect_callback is None:
         spect_callback = spectrogram_ava
     spect, f, t = spect_callback(data, samplerate)
 
     dt = t[1] - t[0]
-    min_syll_len = int(np.floor(min_dur / dt))
-    max_syll_len = int(np.ceil(max_dur / dt))
-    onsets, offsets = [], []
-    too_short, too_long = 0, 0
-
     # Calculate amplitude and smooth.
     if use_softmax_amp:
         amps = softmax(spect, t=temperature)
@@ -174,6 +186,7 @@ def ava_like(
             local_maxima.append(i)
 
     # Then search to the left and right for onsets and offsets.
+    onsets, offsets = [], []
     for local_max in local_maxima:
         # skip this local_max if we are in a region we already declared a syllabel
         if len(offsets) > 1 and local_max < offsets[-1]:
@@ -217,16 +230,14 @@ def ava_like(
             continue
 
     # Throw away syllables that are too long or too short.
+    min_dur_samples = int(np.floor(min_dur / dt))
+    max_dur_samples = int(np.ceil(max_dur / dt))
     new_onsets = []
     new_offsets = []
     for i in range(len(offsets)):
         t1, t2 = onsets[i], offsets[i]
-        if t2 - t1 + 1 <= max_syll_len and t2 - t1 + 1 >= min_syll_len:
+        if t2 - t1 + 1 <= max_dur_samples and t2 - t1 + 1 >= min_dur_samples:
             new_onsets.append(t1 * dt)
             new_offsets.append(t2 * dt)
-        elif t2 - t1 + 1 > max_syll_len:
-            too_long += 1
-        else:
-            too_short += 1
 
     return np.array(new_onsets), np.array(new_offsets)
