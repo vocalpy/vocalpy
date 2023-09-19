29dc01c01435355e35566c8172fb2f455b069b80
David Nicholson <nickledave@users.noreply.github.com>
2023-09-18 20:20:21 -0400

    Write unit test in tests/test_segment/test_ava.py

--- tests/test_segment/test_ava.py
+++ tests/test_segment/test_ava.py
@@ -1,28 +1,10 @@
-import pathlib
+import functools
 
 import numpy as np
 import vocalpy as voc
 
 
-test_data_dir = pathlib.Path(
-    '/home/pimienta/Documents/data/vocal/goffinet'
-)
-wav_dir = test_data_dir / 'BM003'
-
-
-
-wav_paths = voc.paths.from_dir(wav_dir, ext='wav')
-
-
-
-segs_dir = test_data_dir / 'segs'
-
-
-
-seg_txt_files = sorted(segs_dir.glob('*.txt'))
-
-
-spect_params = {
+SPECT_PARAMS = {
     'min_freq': 30000.0,
     'max_freq': 110000.0,
     'nperseg': 1024,
@@ -32,19 +14,7 @@ spect_params = {
     'transform': 'log_magnitude',
 }
 
-
-# In[7]:
-
-
-import functools
-
-spect_callable = functools.partial(voc.segment.ava.get_spectrogram, **spect_params)
-
-
-# In[8]:
-
-
-segment_params = {
+SEGMENT_PARAMS = {
     'thresh_max': 0.305,
     'thresh_min': 0.3,
     'thresh_lowest': 0.295,
@@ -55,18 +25,22 @@ segment_params = {
 }
 
 
-# In[20]:
+def test_ava(goffinet_etal_2021_wav_seg_tuple):
+    """Test that :func:`vocalpy.segment.ava.segment` replicates segmenting results
+    obtained with the ``ava`` package."""
+    wav_path, seg_txt_path = goffinet_etal_2021_wav_seg_tuple
 
+    spect_callable = functools.partial(voc.segment.ava.get_spectrogram, **SPECT_PARAMS)
 
-for seg_txt_path in seg_txt_files:
     segs = np.loadtxt(seg_txt_path)
     segs = segs.reshape(-1,2)
     onsets_txt, offsets_txt = segs[:,0], segs[:,1]
-    wav_path = (wav_dir / seg_txt_path.name.replace('.txt', '.wav'))
+
     audio = voc.Audio.read(wav_path)
     onsets, offsets = voc.segment.ava.segment(audio.data, audio.samplerate,
                                           spect_callback=spect_callable,
-                                          **segment_params)
+                                          **SEGMENT_PARAMS)
+    assert isinstance(onsets, np.ndarray)
+    assert isinstance(offsets, np.ndarray)
     np.allclose(onsets_txt, onsets)
     np.allclose(offsets_txt, offsets)
-
