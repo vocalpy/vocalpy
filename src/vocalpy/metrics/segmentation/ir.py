5f700d12904e5f7e451debb4c80edeb73df5c389
David Nicholson <nickledave@users.noreply.github.com>
2023-09-19 13:37:12 -0400

    Modify src/vocalpy/metrics/segmentation/ir.py to squelch flake8 warnings, will fix this in another branch

--- src/vocalpy/metrics/segmentation/ir.py
+++ src/vocalpy/metrics/segmentation/ir.py
@@ -1,19 +1,14 @@
 """Metrics for segmentation adapted from information retrieval."""
 from __future__ import annotations
 
-import pathlib
-from typing import Sequence
-
-import evfuncs
 import numpy as np
 import numpy.typing as npt
 import scipy.spatial.distance
 
 tol = 0.01
 
-def _compute_tp(
-        boundaries_hyp: npt.NDArray, boundaries_ref: npt.NDArray, tol: float = 0.01
-) -> int:
+
+def _compute_tp(boundaries_hyp: npt.NDArray, boundaries_ref: npt.NDArray, tol: float = 0.01) -> int:
     """Helper function to compute number of true positives.
 
     Parameters
@@ -32,37 +27,29 @@ def _compute_tp(
     tp : int
         The number of true positives.
     """
-    if tol < 0.:
-        raise ValueError(
-            f"Tolerance value ``tol`` must be a non-negative float but was: {tol}"
-        )
+    if tol < 0.0:
+        raise ValueError(f"Tolerance value ``tol`` must be a non-negative float but was: {tol}")
 
     tp = 0
-    for boundary_num, boundary_time_ref in enumerate(boundaries_ref):
+    for _, boundary_time_ref in enumerate(boundaries_ref):
         # FIXME: do we ignore multiple hits here?
-        hits = (np.abs(
-            scipy.spatial.distance.cdist(
-                    np.array([[boundary_time_ref]]), boundaries_hyp[:, np.newaxis]
-                )
-            )
-        < tol).sum()
+        hits = (
+            np.abs(scipy.spatial.distance.cdist(np.array([[boundary_time_ref]]), boundaries_hyp[:, np.newaxis])) < tol
+        ).sum()
         if hits > 0:
             tp += 1
     return tp
 
 
-def compute_tp(onsets_hyp, offsets_hyp, onsets_ref, offsets_ref, tol=0.01, method='combine'):
-    if method == 'combine':
+def compute_tp(onsets_hyp, offsets_hyp, onsets_ref, offsets_ref, tol=0.01, method="combine"):
+    if method == "combine":
         boundaries_hyp = np.concatenate((onsets_hyp, offsets_hyp))
         boundaries_ref = np.concatenate((onsets_ref, offsets_ref))
         tp, all_hits = _compute_tp(boundaries_hyp, boundaries_ref)
-    elif method == 'separate':
+    elif method == "separate":
         tp = 0
         all_hits = []
-        for boundaries_hyp, boundaries_ref in zip(
-            (onsets_hyp, offsets_hyp),
-            (onsets_ref, offsets_ref)
-        ):
+        for boundaries_hyp, boundaries_ref in zip((onsets_hyp, offsets_hyp), (onsets_ref, offsets_ref)):
             tp_, all_hits_ = _compute_tp(boundaries_hyp, boundaries_ref)
             tp += tp_
             all_hits.append(all_hits_)
@@ -72,7 +59,11 @@ def compute_tp(onsets_hyp, offsets_hyp, onsets_ref, offsets_ref, tol=0.01, metho
 def _precision(tp, pp):
     return tp / pp
 
-def precision()
+
+# FIXME
+def precision():
+    pass
+
 
 def _recall(tp, p):
     return tp / p
