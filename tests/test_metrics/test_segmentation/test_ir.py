2bc423fa323c5e10aaa43c94c5556ae12d6dbbc9
David Nicholson <nickledave@users.noreply.github.com>
2023-09-21 16:54:10 -0400

    Add initial tests for precision and recall to tests/test_metrics/test_segmentation/test_ir.py

--- tests/test_metrics/test_segmentation/test_ir.py
+++ tests/test_metrics/test_segmentation/test_ir.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import pytest
 
@@ -15,6 +17,30 @@ import vocalpy.metrics.segmentation.ir
             4,
             np.array([0, 1, 2, 3]),
         ),
+        (
+                np.array([0, 5, 10, 15]),
+                np.array([1, 6, 11, 16]),
+                1,
+                'default',
+                4,
+                np.array([0, 1, 2, 3]),
+        ),
+        (
+                np.array([0., 5., 10., 15.]),
+                np.array([0., 5., 10., 15.]),
+                'default',
+                'default',
+                4,
+                np.array([0, 1, 2, 3]),
+        ),
+        (
+                np.array([0., 5., 10., 15.]),
+                np.array([0.5, 5.5, 10.5, 15.5]),
+                0.5,
+                'default',
+                4,
+                np.array([0, 1, 2, 3]),
+        ),
     ]
 )
 def test_compute_true_positives(hypothesis, reference, tolerance, decimals, expected_n_tp, expected_hits):
@@ -22,5 +48,116 @@ def test_compute_true_positives(hypothesis, reference, tolerance, decimals, expe
         n_tp, hits = vocalpy.metrics.segmentation.ir.compute_true_positives(
             hypothesis, reference
         )
+        assert n_tp == expected_n_tp
+        assert np.array_equal(hits, expected_hits)
+
+
+@pytest.mark.parametrize(
+    'hypothesis, reference, tolerance, decimals, expected_precision, expected_n_tp, expected_hits',
+    [
+        (
+            np.array([0, 5, 10, 15]),
+            np.array([0, 5, 10, 15]),
+            'default',
+            'default',
+            1.0,
+            4,
+            np.array([0, 1, 2, 3]),
+        ),
+        (
+                np.array([1, 6, 11, 16]),
+                np.array([0, 5, 10, 15]),
+                'default',
+                'default',
+                0.0,
+                0,
+                np.array([]),
+        ),
+        (
+                np.array([1, 6, 11, 16]),
+                np.array([0, 5, 10, 15]),
+                1,
+                'default',
+                1.0,
+                4,
+                np.array([0, 1, 2, 3]),
+        ),
+        (
+                np.array([0., 5., 10., 15.]),
+                np.array([0., 5., 10., 15.]),
+                'default',
+                'default',
+                1.0,
+                4,
+                np.array([0, 1, 2, 3]),
+        ),
+        (
+                np.array([0., 5., 10., 15.]),
+                np.array([0.5, 5.5, 10.5, 15.5]),
+                0.5,
+                'default',
+                1.0,
+                4,
+                np.array([0, 1, 2, 3]),
+        ),
+    ]
+)
+def test___precision(hypothesis, reference, tolerance, decimals, expected_precision, expected_n_tp, expected_hits):
+    if tolerance == 'default' and decimals == 'default':
+        precision, n_tp, hits = vocalpy.metrics.segmentation.ir._precision(
+            hypothesis, reference
+        )
+        assert math.isclose(precision, expected_precision)
+        assert n_tp == expected_n_tp
+        assert np.array_equal(hits, expected_hits)
+
+
+@pytest.mark.parametrize(
+    'hypothesis, reference, tolerance, decimals, expected_recall, expected_n_tp, expected_hits',
+    [
+        (
+            np.array([0, 5, 10, 15]),
+            np.array([0, 5, 10, 15]),
+            'default',
+            'default',
+            1.0,
+            4,
+            np.array([0, 1, 2, 3]),
+        ),
+        (
+                np.array([0, 5, 10, 15]),
+                np.array([1, 6, 11, 16]),
+                1,
+                'default',
+                1.0,
+                4,
+                np.array([0, 1, 2, 3]),
+        ),
+        (
+                np.array([0., 5., 10., 15.]),
+                np.array([0., 5., 10., 15.]),
+                'default',
+                'default',
+                1.0,
+                4,
+                np.array([0, 1, 2, 3]),
+        ),
+        (
+                np.array([0., 5., 10., 15.]),
+                np.array([0.5, 5.5, 10.5, 15.5]),
+                0.5,
+                'default',
+                1.0,
+                4,
+                np.array([0, 1, 2, 3]),
+        ),
+    ]
+)
+def test___recall(hypothesis, reference, tolerance, decimals, expected_recall, expected_n_tp, expected_hits):
+    if tolerance == 'default' and decimals == 'default':
+        recall, n_tp, hits = vocalpy.metrics.segmentation.ir._recall(
+            hypothesis, reference
+        )
+        assert math.isclose(recall, expected_recall)
         assert n_tp == expected_n_tp
         assert np.array_equal(hits, expected_hits)
