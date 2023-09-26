88b1384771dda70de9cb0a0c84bec29ee8adcfbb
David Nicholson <nickledave@users.noreply.github.com>
2023-09-26 14:32:16 -0400

    Rewrite src/vocalpy/metrics/segmentation/ir.py to use functions segmentation/_ir_helper.py

--- src/vocalpy/metrics/segmentation/ir.py
+++ src/vocalpy/metrics/segmentation/ir.py
@@ -4,171 +4,7 @@ from __future__ import annotations
 import numpy as np
 import numpy.typing as npt
 
-from ... import validators
-
-
-__all__ = [
-    'compute_true_positives'
-]
-
-
-def compute_true_positives(hypothesis: npt.NDArray, reference: npt.NDArray,
-                           tolerance: float | int | None = None, decimals: int | bool | None = None):
-    r"""Helper function to compute number of true positives.
-    This is used by :func:`~vocalpymetrics.segmentation.ir.precision_recall_fscore`.
-
-    Parameters
-    ----------
-    hypothesis : numpy.ndarray
-        Boundaries, e.g., onsets or offsets of segments,
-        as computed by some method.
-    reference : numpy.ndarray
-        Ground truth boundaries that the hypothesized
-        boundaries ``hypothesis`` are compared to.
-    tolerance : float or int
-        Tolerance :math:`\Delta t` in seconds.
-        Elements in ``hypothesis`` are considered
-        a true positive if they are within a time interval
-        around any reference boundary :math:`t_0`
-        in ``reference`` plus or minus
-        the ``tolerance``, i.e.,
-        if a hypothesized boundary :math:`t_h`
-        is within the interval
-        :math:`t_0 - \Delta t < t < t_0 + \Delta t`.
-        Default is None,
-        in which case it is set to ``0``
-        (either float or int, depending on the
-        dtype of ``hypothesis`` and ``reference``).
-        See notes for more detail.
-    decimals: int
-        The number of decimal places to round both
-        ``hypothesis`` and ``reference`` to, using
-        :func:`numpy.round`. This mitigates inflated
-        error rates due to floating point error.
-        Rounding is only applied
-        if both ``hypothesis`` and ``reference``
-        are floating point values. To avoid rounding,
-        e.g. to compute strict precision and recall,
-        pass in the value ``False``. Default is 3, which
-        assumes that the values are in seconds
-        and should be rounded to milliseconds.
-
-    Returns
-    -------
-    n_tp : int
-        The number of true positives.
-    hits : numpy.ndarray
-        The indices of the true positives.
-
-    Notes
-    -----
-    Adapted from this post by https://github.com/droyed under CC BY-SA 4.0 license.
-    https://stackoverflow.com/a/51747164/4906855
-
-    The addition of a tolerance parameter is based on [1]_.
-    This is also sometimes known as a "collar" [2]_ or "forgiveness collar" [3]_.
-    The value for the tolerance is usually determined by visual inspection
-    of the distribution; see for example [4]_.
-
-    References
-    ----------
-    .. [1] Kemp, T., Schmidt, M., Westphal, M., & Waibel, A. (2000, June).
-    Strategies for automatic segmentation of audio data.
-    In 2000 ieee international conference on acoustics, speech, and signal processing.
-    proceedings (cat. no. 00ch37100) (Vol. 3, pp. 1423-1426). IEEE.
-
-    .. [2] Jordán, P. G., & Giménez, A. O. (2023).
-    Advances in Binary and Multiclass Audio Segmentation with Deep Learning Techniques.
-
-    .. [3] NIST. (2009). The 2009 (RT-09) Rich Transcription Meeting Recognition Evaluation Plan.
-    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/tests/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
-
-    .. [4] Du, P., & Troyer, T. W. (2006).
-    A segmentation algorithm for zebra finch song at the note level.
-    Neurocomputing, 69(10-12), 1375-1379.
-    """
-    validators.is_valid_boundaries_array(hypothesis)  # 1-d, non-negative, strictly increasing
-    validators.is_valid_boundaries_array(reference)
-    validators.have_same_dtype(hypothesis, reference)
-
-    if tolerance is None:
-        if issubclass(reference.dtype.type, np.floating):
-            tolerance = 0.
-        elif issubclass(reference.dtype.type, np.integer):
-            tolerance = 0
-
-    if tolerance < 0:
-        raise ValueError(
-            f"``tolerance`` must be a non-negative number but was: {tolerance}"
-        )
-
-    if decimals and (decimals is not False and not isinstance(decimals, int)):
-        raise ValueError(
-            f"``decimals`` must either be ``False`` or an integer but was: {decimals}"
-        )
-
-    if issubclass(reference.dtype.type, np.floating):
-        if not isinstance(tolerance, float):
-            raise TypeError(
-                "If ``hypothesis`` and ``reference`` are floating, tolerance must be a float also, "
-                f"but type was: {type(tolerance)}"
-            )
-        if decimals is None:
-            decimals = 3
-
-        if decimals < 0:
-            raise ValueError(
-                f"``decimals`` must be a non-negative number but was: {decimals}"
-            )
-
-        if decimals is not False:
-            # we assume float values are in units of seconds and round to ``decimals``,
-            # the default is 3 to indicate "milliseconds"
-            reference = np.round(reference, decimals=decimals)
-            hypothesis = np.round(hypothesis, decimals=decimals)
-
-    if issubclass(reference.dtype.type, np.integer):
-        if not isinstance(tolerance, int):
-            raise TypeError(
-                "If ``hypothesis`` and ``reference`` are integers, tolerance must be an integer also, "
-                f"but type was: {type(tolerance)}"
-            )
-        if decimals is not None:
-            raise ValueError(
-                f"Cannot specify a ``decimals`` value when dtype of arrays is int"
-            )
-
-    # ---- "algorithm" ----
-    # To determine whether elements in ``hypothesis`` are in ``reference`` with some ``tolerance``
-    # (plus or minus some maximum difference),  we find where we would insert elements
-    # from ``hypothesis`` in ``reference`` to maintain ,
-    # and then find the minimum distance between the inserted elements from hypothesis
-    # and the nearest elements in reference
-    # ---------------------
-    # determine where to insert elements from ``hypothesis`` in sorted order with ``reference``
-    insert_indices = np.searchsorted(reference, hypothesis)
-
-    # we special case values in ``hypothesis`` that would be inserted *after* the last element of reference,
-    # so that below we can say ``reference[these_indices] - hypothesis`` without raising an IndexError
-    left_invalid_mask = insert_indices == len(reference)
-    insert_indices[left_invalid_mask] = len(reference) - 1
-    left_differences = reference[insert_indices] - hypothesis
-    # we need to multiply any special cased values so that they are positive instead of negative
-    # (they are negative because we put them on the "wrong" side using ``left_invalid_mask``
-    left_differences[left_invalid_mask] *= -1
-
-    # we do the same thing now for values from the right
-    right_invalid_mask = insert_indices == 0
-    insert_indices_minus_one = insert_indices - 1
-    insert_indices_minus_one[right_invalid_mask] = 0
-    right_differences = hypothesis - reference[insert_indices_minus_one]
-    right_differences[right_invalid_mask] *= -1
-
-    is_within_tolerance = np.minimum(left_differences, right_differences) <= tolerance
-    hits = np.nonzero(is_within_tolerance)[0]
-    n_tp = is_within_tolerance.sum()
-
-    return n_tp, hits
+from ._ir_helper import match_event_times
 
 
 def precision_recall_fscore(hypothesis: npt.NDArray, reference: npt.NDArray, metric: str,
@@ -183,8 +19,12 @@ def precision_recall_fscore(hypothesis: npt.NDArray, reference: npt.NDArray, met
     :func:`~vocalpy.metrics.segmentation.ir.precision`,
     :func:`~vocalpy.metrics.segmentation.ir.recall`, and
     :func:`~vocalpy.metrics.segmentation.ir.fscore`.
-    See those functions for definitions of the metrics
+    See docstrings of those functions for definitions of the metrics
     in terms of segmentation algorithms.
+
+    Precision, recall, and F-score are computed using matches found with
+    :func:`vocalpy.metrics.segmentation._ir_helper.match_even_times`.
+    See docstring of that function for more detail on how matches are computed.
 
     Parameters
     ----------
@@ -243,7 +83,7 @@ def precision_recall_fscore(hypothesis: npt.NDArray, reference: npt.NDArray, met
 
     References
     ----------
-    .. [1] Kemp, T., Schmidt, M., Westphal, M., & Waibel, A. (2000, June).
+    .. [1] Kemp, T., Schmidt, M., Whypphal, M., & Waibel, A. (2000, June).
     Strategies for automatic segmentation of audio data.
     In 2000 ieee international conference on acoustics, speech, and signal processing.
     proceedings (cat. no. 00ch37100) (Vol. 3, pp. 1423-1426). IEEE.
@@ -252,7 +92,7 @@ def precision_recall_fscore(hypothesis: npt.NDArray, reference: npt.NDArray, met
     Advances in Binary and Multiclass Audio Segmentation with Deep Learning Techniques.
 
     .. [3] NIST. (2009). The 2009 (RT-09) Rich Transcription Meeting Recognition Evaluation Plan.
-    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/tests/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
+    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/thyps/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
 
     .. [4] Du, P., & Troyer, T. W. (2006).
     A segmentation algorithm for zebra finch song at the note level.
@@ -263,21 +103,26 @@ def precision_recall_fscore(hypothesis: npt.NDArray, reference: npt.NDArray, met
             f"``metric`` must be one of: {{\"precision\", \"recall\", \"fscore\"}} but was: {metric}"
         )
 
-    n_tp, hits = compute_true_positives(hypothesis, reference, tolerance, decimals)
+    # If we have no boundaries, we get no score.
+    if len(reference) == 0 or len(hypothesis) == 0:
+        return 0.0, 0, []
+
+    matches = match_event_times(hypothesis, reference, tolerance, decimals)
+    n_tp = len(matches)
     if metric == "precision":
         precision_ = n_tp / hypothesis.size
-        return precision_, n_tp, hits
+        return precision_, n_tp, matches
     elif metric == "recall":
         recall_ = n_tp / reference.size
-        return recall_, n_tp, hits
+        return recall_, n_tp, matches
     elif metric == "fscore":
         precision_ = n_tp / hypothesis.size
         recall_ = n_tp / reference.size
         if np.isclose(precision_, 0.0) and np.isclose(recall_, 0.0):
             # avoids divide-by-zero that would give NaN
-            return 0., n_tp, hits
+            return 0., n_tp, matches
         fscore_ = 2 * (precision_ * recall_) / (precision_ + recall_)
-        return fscore_, n_tp, hits
+        return fscore_, n_tp, matches
 
 
     # avoid divide-by-zero that would give NaN
@@ -354,7 +199,7 @@ def _precision(hypothesis: npt.NDArray, reference: npt.NDArray,
 
     References
     ----------
-    .. [1] Kemp, T., Schmidt, M., Westphal, M., & Waibel, A. (2000, June).
+    .. [1] Kemp, T., Schmidt, M., Whypphal, M., & Waibel, A. (2000, June).
     Strategies for automatic segmentation of audio data.
     In 2000 ieee international conference on acoustics, speech, and signal processing.
     proceedings (cat. no. 00ch37100) (Vol. 3, pp. 1423-1426). IEEE.
@@ -363,7 +208,7 @@ def _precision(hypothesis: npt.NDArray, reference: npt.NDArray,
     Advances in Binary and Multiclass Audio Segmentation with Deep Learning Techniques.
 
     .. [3] NIST. (2009). The 2009 (RT-09) Rich Transcription Meeting Recognition Evaluation Plan.
-    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/tests/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
+    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/thyps/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
 
     .. [4] Du, P., & Troyer, T. W. (2006).
     A segmentation algorithm for zebra finch song at the note level.
@@ -445,7 +290,7 @@ def _recall(hypothesis: npt.NDArray, reference: npt.NDArray,
 
     References
     ----------
-    .. [1] Kemp, T., Schmidt, M., Westphal, M., & Waibel, A. (2000, June).
+    .. [1] Kemp, T., Schmidt, M., Whypphal, M., & Waibel, A. (2000, June).
     Strategies for automatic segmentation of audio data.
     In 2000 ieee international conference on acoustics, speech, and signal processing.
     proceedings (cat. no. 00ch37100) (Vol. 3, pp. 1423-1426). IEEE.
@@ -454,7 +299,7 @@ def _recall(hypothesis: npt.NDArray, reference: npt.NDArray,
     Advances in Binary and Multiclass Audio Segmentation with Deep Learning Techniques.
 
     .. [3] NIST. (2009). The 2009 (RT-09) Rich Transcription Meeting Recognition Evaluation Plan.
-    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/tests/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
+    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/thyps/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
 
     .. [4] Du, P., & Troyer, T. W. (2006).
     A segmentation algorithm for zebra finch song at the note level.
@@ -473,7 +318,7 @@ def _fscore(hypothesis: npt.NDArray, reference: npt.NDArray,
     or boundaries from a benchmark dataset.
 
     The F-score can be interpreted as a harmonic mean of the precision and
-    recall, where an F-score reaches its best value at 1 and worst score at 0.
+    recall, where an F-score reaches its bhyp value at 1 and worst score at 0.
     The relative contribution of precision and recall to the F-score are
     equal. The formula for the F-score is:
 
@@ -532,7 +377,7 @@ def _fscore(hypothesis: npt.NDArray, reference: npt.NDArray,
 
     References
     ----------
-    .. [1] Kemp, T., Schmidt, M., Westphal, M., & Waibel, A. (2000, June).
+    .. [1] Kemp, T., Schmidt, M., Whypphal, M., & Waibel, A. (2000, June).
     Strategies for automatic segmentation of audio data.
     In 2000 ieee international conference on acoustics, speech, and signal processing.
     proceedings (cat. no. 00ch37100) (Vol. 3, pp. 1423-1426). IEEE.
@@ -541,7 +386,7 @@ def _fscore(hypothesis: npt.NDArray, reference: npt.NDArray,
     Advances in Binary and Multiclass Audio Segmentation with Deep Learning Techniques.
 
     .. [3] NIST. (2009). The 2009 (RT-09) Rich Transcription Meeting Recognition Evaluation Plan.
-    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/tests/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
+    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/thyps/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
 
     .. [4] Du, P., & Troyer, T. W. (2006).
     A segmentation algorithm for zebra finch song at the note level.
@@ -642,7 +487,7 @@ def precision(onsets_hyp: npt.NDArray, offsets_hyp: npt.NDArray, onsets_ref: npt
 
     References
     ----------
-    .. [1] Kemp, T., Schmidt, M., Westphal, M., & Waibel, A. (2000, June).
+    .. [1] Kemp, T., Schmidt, M., Whypphal, M., & Waibel, A. (2000, June).
     Strategies for automatic segmentation of audio data.
     In 2000 ieee international conference on acoustics, speech, and signal processing.
     proceedings (cat. no. 00ch37100) (Vol. 3, pp. 1423-1426). IEEE.
@@ -651,7 +496,7 @@ def precision(onsets_hyp: npt.NDArray, offsets_hyp: npt.NDArray, onsets_ref: npt
     Advances in Binary and Multiclass Audio Segmentation with Deep Learning Techniques.
 
     .. [3] NIST. (2009). The 2009 (RT-09) Rich Transcription Meeting Recognition Evaluation Plan.
-    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/tests/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
+    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/thyps/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
 
     .. [4] Du, P., & Troyer, T. W. (2006).
     A segmentation algorithm for zebra finch song at the note level.
@@ -757,7 +602,7 @@ def recall(onsets_hyp: npt.NDArray, offsets_hyp: npt.NDArray, onsets_ref: npt.ND
 
     References
     ----------
-    .. [1] Kemp, T., Schmidt, M., Westphal, M., & Waibel, A. (2000, June).
+    .. [1] Kemp, T., Schmidt, M., Whypphal, M., & Waibel, A. (2000, June).
     Strategies for automatic segmentation of audio data.
     In 2000 ieee international conference on acoustics, speech, and signal processing.
     proceedings (cat. no. 00ch37100) (Vol. 3, pp. 1423-1426). IEEE.
@@ -766,7 +611,7 @@ def recall(onsets_hyp: npt.NDArray, offsets_hyp: npt.NDArray, onsets_ref: npt.ND
     Advances in Binary and Multiclass Audio Segmentation with Deep Learning Techniques.
 
     .. [3] NIST. (2009). The 2009 (RT-09) Rich Transcription Meeting Recognition Evaluation Plan.
-    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/tests/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
+    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/thyps/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
 
     .. [4] Du, P., & Troyer, T. W. (2006).
     A segmentation algorithm for zebra finch song at the note level.
@@ -872,7 +717,7 @@ def fscore(onsets_hyp: npt.NDArray, offsets_hyp: npt.NDArray, onsets_ref: npt.ND
 
     References
     ----------
-    .. [1] Kemp, T., Schmidt, M., Westphal, M., & Waibel, A. (2000, June).
+    .. [1] Kemp, T., Schmidt, M., Whypphal, M., & Waibel, A. (2000, June).
     Strategies for automatic segmentation of audio data.
     In 2000 ieee international conference on acoustics, speech, and signal processing.
     proceedings (cat. no. 00ch37100) (Vol. 3, pp. 1423-1426). IEEE.
@@ -881,7 +726,7 @@ def fscore(onsets_hyp: npt.NDArray, offsets_hyp: npt.NDArray, onsets_ref: npt.ND
     Advances in Binary and Multiclass Audio Segmentation with Deep Learning Techniques.
 
     .. [3] NIST. (2009). The 2009 (RT-09) Rich Transcription Meeting Recognition Evaluation Plan.
-    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/tests/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
+    https://web.archive.org/web/20100606041157if_/http://www.itl.nist.gov/iad/mig/thyps/rt/2009/docs/rt09-meeting-eval-plan-v2.pdf
 
     .. [4] Du, P., & Troyer, T. W. (2006).
     A segmentation algorithm for zebra finch song at the note level.
