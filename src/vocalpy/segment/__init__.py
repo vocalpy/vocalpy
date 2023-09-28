b5481055d686847e4232f11e871eb745f75fe8d1
David Nicholson <nickledave@users.noreply.github.com>
2023-09-27 22:23:43 -0400

    Add autosummary to module-level docstring in src/vocalpy/segment/__init__.py

--- src/vocalpy/segment/__init__.py
+++ src/vocalpy/segment/__init__.py
@@ -6,6 +6,12 @@ with start and stop times,
 and algorithms that find bounding boxes in spectrograms
 with high and low frequencies in addition
 to the start and stop times.
+
+.. autosummary::
+   :toctree: generated/
+
+   audio_amplitude
+   ava
 """
 from . import ava
 from .audio_amplitude import audio_amplitude
