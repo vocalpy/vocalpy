10b88cb388b11e883be03c12267c93f4206b7ff9
David Nicholson <nickledave@users.noreply.github.com>
2023-09-29 12:34:55 -0400

    Fix test for smoothed_energy in tests/test_signal/test_audio.py

--- tests/test_signal/test_audio.py
+++ tests/test_signal/test_audio.py
@@ -3,9 +3,8 @@ import numpy as np
 import vocalpy
 
 
-def test_smooth(an_audio_path):
+def test_smoothed_energy(an_audio_path):
     audio = vocalpy.Audio.read(an_audio_path)
-    dat, fs = audio.data, audio.samplerate
-    smoothed = vocalpy.signal.audio.smooth(dat, fs)
+    smoothed = vocalpy.signal.audio.smoothed_energy(audio)
     assert isinstance(smoothed, np.ndarray)
     assert np.all(smoothed >= 0.)  # because it's rectified
