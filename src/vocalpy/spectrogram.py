8710811c714a069633820f0c225f2db57cda6c87
David Nicholson <nickledave@users.noreply.github.com>
2023-12-30 08:45:34 -0500

    Add to docstring of spectrogram.spectrogram

--- src/vocalpy/spectrogram.py
+++ src/vocalpy/spectrogram.py
@@ -9,18 +9,43 @@ from ._spectrogram.data_type import Spectrogram
 
 
 # TODO: add 'SAT' method and 'biosound' method
-def spectrogram(audio: Audio, n_fft: int = 512, hop_length: int = 64, method='librosa-db') -> Spectrogram:
-    """Generate a spectrogram from audio.
+def spectrogram(audio: Audio, n_fft: int = 512, hop_length: int = 64, method='librosa-db', **kwargs) -> Spectrogram:
+    """Get a spectrogram from audio.
+
+    This is a convenience function that takes an instance of :class:`vocalpy.Audio`
+    and returns an instance of :class:`vocalpy.Spectrogram`. The
+    :attr:`vocalpy.Spectrogram.data` will be a spectral representation
+    computed according to the specified `method`.
+
+    Methods
+    =======
+    * `'librosa-db`': equivalent to calling ``S = librosa.STFT(audio.data)``
+       and then ``S = librosa.amplitude_to_db(np.abs(S))``.
 
     Parameters
     ----------
-    audio :
+    audio : vocalpy.Audio
+        Audio used to compute spectrogram.
     n_fft : int
+        Length of the frame used for the Fast Fourier Transform,
+        in number of audio samples. Default is 512.
     hop_length : int
+        Number of audio samples to "hop" for each frame
+        whe computing the Fast Fourier Transform.
+        Smaller values increase the number of columns in the spectrogram,
+        without affecting the frequency resolution of the STFT.
     method : str
+        Name  of method.
+        Default is `'librosa-db'`.
 
     Returns
     -------
+    spect : vocalpy.Spectrogram
+        A :class:`vocalpy.Spectrogram` instance
+        computed according to `method`
+
+    Examples
+    --------
 
     """
     if not isinstance(audio, Audio):
