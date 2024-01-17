4c8db614ea2a961604c2c6c70f34cf899ce96e01
David Nicholson <nickledave@users.noreply.github.com>
2024-01-16 21:15:22 -0500

    Finish/add tests in tests/test_spectrogram.py

--- tests/test_spectrogram.py
+++ tests/test_spectrogram.py
@@ -1,7 +1,35 @@
+import pytest
+
 import vocalpy
 
+from .fixtures.audio import AUDIO_LIST_WAV
 
-def test_spectrogram(a_wav_path):
+
+@pytest.mark.parametrize(
+    'method',
+    [
+        None,
+        'librosa-db',
+    ]
+)
+def test_spectrogram(method, a_wav_path):
     audio = vocalpy.Audio.read(a_wav_path)
-    spectrogram = vocalpy.spectrogram(audio)
+    if method is not None:
+        spectrogram = vocalpy.spectrogram(audio, method=method)
+    else:
+        spectrogram = vocalpy.spectrogram(audio)
     assert isinstance(spectrogram, vocalpy.Spectrogram)
+
+
+def test_input_not_audio_raises():
+    """Test :func:`vocalpy.spectrogram` raises ValueError when first arg is not Audio"""
+    audio = vocalpy.Audio.read(AUDIO_LIST_WAV[0])
+    with pytest.raises(TypeError):
+        vocalpy.spectrogram(audio.data)
+
+
+def test_method_not_valid_raises():
+    """Test :func:`vocalpy.spectrogram` raises ValueError when method arg is not valid"""
+    audio = vocalpy.Audio.read(AUDIO_LIST_WAV[0])
+    with pytest.raises(ValueError):
+        vocalpy.spectrogram(audio, method='incorrect-method-name')
