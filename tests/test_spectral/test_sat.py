b2104763b8a732e70687abc86ecfb14e6d80b916
David Nicholson <nickledave@users.noreply.github.com>
2024-02-08 21:02:09 -0500

    Test multi-channel audio in tests/test_spectral/test_sat.py

--- tests/test_spectral/test_sat.py
+++ tests/test_spectral/test_sat.py
@@ -1,15 +1,27 @@
 import numpy as np
+import pytest
 
 import vocalpy
 
+from ..fixtures.audio import ALL_ZEBRA_FINCH_WAVS, MULTICHANNEL_FLY_WAV
 
-def test_sat(a_zebra_finch_wav):
-    """Smoke test that tests :func:`vocalpy.spectral.sat` returns expected outputs"""
-    audio = vocalpy.Sound.read(a_zebra_finch_wav)
-    out = vocalpy.spectral.sat(audio)
+
+@pytest.mark.parametrize(
+    'audio_path',
+    ALL_ZEBRA_FINCH_WAVS + [MULTICHANNEL_FLY_WAV]
+)
+def test_sat(audio_path):
+    """Test :func:`vocalpy.spectral.sat` returns expected outputs"""
+    sound = vocalpy.Sound.read(audio_path)
+    out = vocalpy.spectral.sat(sound)
     assert len(out) == 6
     power_spectrogram, cepstrogram, quefrencies, max_freq, dSdt, dSdf = out
     assert isinstance(power_spectrogram, vocalpy.Spectrogram)
-    for arr in (cepstrogram, quefrencies, dSdt, dSdf):
+    assert power_spectrogram.data.shape[0] == sound.data.shape[0]
+    assert power_spectrogram.data.ndim == 3
+    assert isinstance(quefrencies, np.ndarray)
+    for arr in (cepstrogram, dSdt, dSdf):
         assert isinstance(arr, np.ndarray)
+        assert arr.ndim == 3
+        assert arr.shape[0] == sound.data.shape[0]
     assert isinstance(max_freq, float)
