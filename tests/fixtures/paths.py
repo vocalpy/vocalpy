ca712e3adfc33348e8b362278553874dfe8a0d90
David Nicholson <nickledave@users.noreply.github.com>
2024-02-20 09:45:25 -0500

    Add tests/fixtures/paths.py

---
+++ tests/fixtures/paths.py
@@ -0,0 +1,6 @@
+"""Fixtures for testing vocalpy.paths"""
+from .test_data import GENERATED_TEST_DATA_ROOT
+
+
+PATHS_DIR = GENERATED_TEST_DATA_ROOT / 'paths'
+DATA_ROOTS_WITH_SUBDIRS = PATHS_DIR / 'root_with_subdirs_to_test_paths'
