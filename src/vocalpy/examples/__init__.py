f43c1f051f623b89e472fa2906fe51c44f19ba8b
David Nicholson <nickledave@users.noreply.github.com>
2024-02-21 19:17:31 -0500

    Add/fix imports in src/vocalpy/examples/__init__.py

--- src/vocalpy/examples/__init__.py
+++ src/vocalpy/examples/__init__.py
@@ -1,7 +1,10 @@
 """Example data built into VocalPy."""
-from ._registry import example, list
+from ._examples import clear_cache, get_cache_dir, example, show
+
 
 __all__ = [
+    "clear_cache",
+    "get_cache_dir",
     "example",
-    "list",
+    "show",
 ]
