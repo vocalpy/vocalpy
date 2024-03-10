9881f56a838adc1a389d033b9dc3336a9cda33b1
David Nicholson <nickledave@users.noreply.github.com>
2024-03-10 17:40:09 -0400

    Add dataclass MeanSquaredParams to src/vocalpy/segment/meansquared.py

--- src/vocalpy/segment/meansquared.py
+++ src/vocalpy/segment/meansquared.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from dataclasses import asdict, dataclass
 from typing import TYPE_CHECKING, Iterable
 
 import numpy as np
@@ -11,6 +12,71 @@ from ..segments import Segments
 
 if TYPE_CHECKING:
     from .. import Sound
+
+
+@dataclass
+class MeanSquaredParams:
+    """Data class that represents parameters
+    for :func:`vocalpy.segment.ava.segment`.
+
+    Attributes
+    ----------
+    threshold : int
+        Value above which mean squared signal is considered part of a segment.
+        Default is 5000.
+    min_dur : float
+        Minimum duration of a segment, in seconds.
+        Default is 0.02, i.e. 20 ms.
+    min_silent_dur : float
+        Minimum duration of silent gap between segments, in seconds.
+        Default is 0.002, i.e. 2 ms.
+    freq_cutoffs : Iterable
+        Cutoff frequencies for bandpass filter.
+        List or tuple with two elements, default is ``(500, 10000)``.
+    smooth_win : int
+        Size of smoothing window in milliseconds. Default is 2.
+    scale : bool
+        If True, scale the ``sound.data``.
+        Default is True.
+        This is needed to replicate the behavior of ``evsonganaly``,
+        which assumes the audio data is loaded as 16-bit integers.
+        Since the default for :class:`vocalpy.Sound` is to load sounds
+        with a numpy dtype of float64, this function defaults to
+        multiplying the ``sound.data`` by 2**15,
+        and then casting to the int16 dtype.
+        This replicates the behavior of the ``evsonganaly`` function,
+        given data with dtype float64.
+        If you have loaded a sound with a dtype of int16,
+        then set this to False.
+    scale_val :
+        Value to multiply the ``sound.data`` by, to scale the data.
+        Default is 2**15.
+        Only used if ``scale`` is ``True``.
+        This is needed to replicate the behavior of ``evsonganaly``,
+        which assumes the audio data is loaded as 16-bit integers.
+    scale_dtype : numpy.dtype
+        Numpy Dtype to cast ``sound.data`` to, after scaling.
+        Default is ``np.int16``.
+        Only used if ``scale`` is ``True``.
+        This is needed to replicate the behavior of ``ava``,
+        which assumes the audio data is loaded as 16-bit integers.
+    """
+    threshold: int = 5000
+    min_dur: float = 0.02
+    min_silent_dur: float = 0.002
+    freq_cutoffs: Iterable = (500, 10000)
+    smooth_win: int = 2
+    scale: bool = True
+    scale_val: int | float = 2 ** 15
+    scale_dtype: npt.DTypeLike = np.int16
+
+    def keys(self):
+        return asdict(self).keys()
+
+    def __getitem__(self, item):
+        if getattr(self, "_dict", None) is None:
+            self._dict = asdict(self)
+        return self._dict[item]
 
 
 def meansquared(
