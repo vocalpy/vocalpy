044ac709b2b0bed14bb975548d9e04cc460228dc
David Nicholson <nickledave@users.noreply.github.com>
2024-04-09 19:28:02 -0400

    WIP: Add FeatureExtractor

---
+++ src/vocalpy/feature_extractor.py
@@ -0,0 +1,78 @@
+from __future__ import annotations
+
+import collections.abc
+import inspect
+import pathlib
+from typing import Mapping, Union, TYPE_CHECKING
+
+import dask
+import dask.diagnostics
+
+
+if TYPE_CHECKING:
+    from . import Features, Params, Sound, Segment
+    FeatureSource = Union[
+        Sound,
+        list[Sound],
+        Segment,
+        list[Segment]
+    ]
+
+
+class FeatureExtractor:
+    def __init__(self, callback: callable, params: dict | Params | None = None):
+        if not callable(callback):
+            raise ValueError(
+                f"`callback` should be callable, but `callable({callback})` returns False"
+            )
+
+        self.callback = callback
+
+        if params is None:
+            params = {}
+            signature = inspect.signature(callback)
+            for name, param in signature.parameters.items():
+                if param.default is not inspect._empty:
+                    params[name] = param.default
+
+        if not isinstance(params, (collections.abc.Mapping, vocalpy.Params)):
+            raise TypeError(f"`params` should be a `Mapping` or `Params` but type was: {type(params)}")
+
+        if isinstance(params, vocalpy.Params):
+            # coerce to dict
+            params = {**params}
+
+        signature = inspect.signature(callback)
+        if not all([param in signature.parameters for param in params]):
+            invalid_params = [param for param in params if param not in signature.parameters]
+            raise ValueError(
+                f"Invalid params for callback: {invalid_params}\n" f"Callback parameters are: {signature.parameters}"
+            )
+
+        self.params = params
+
+    def extract(self, source: FeatureSource, parallelize: bool = True) -> Features:
+
+        # define nested function so vars are in scope and ``dask`` can call it
+        def _to_features(source_: FeatureSource) -> Features:
+            if isinstance(sound_, AudioFile):
+                source_ = Sound.read(sound_.path)
+            segments = self.callback(source_, **self.params)
+            return segments
+
+        if isinstance(source, (vocalpy.Sound, vocalpy.Segments):
+            return _to_features(source)
+
+        features = []
+        for source_ in source:
+            if parallelize:
+                features.append(dask.delayed(_to_features(source)))
+            else:
+                features.append(_to_features(source))
+
+        if parallelize:
+            graph = dask.delayed()(features)
+            with dask.diagnostics.ProgressBar():
+                return graph.compute()
+        else:
+            return features
