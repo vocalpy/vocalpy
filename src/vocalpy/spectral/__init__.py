c9ef1171e09d0fa1167e536f32a1f09a9c35dd87
David Nicholson <nickledave@users.noreply.github.com>
2024-04-11 12:34:33 -0400

    Add module spectral/soundsig.py with function soundsig_spectro

--- src/vocalpy/spectral/__init__.py
+++ src/vocalpy/spectral/__init__.py
@@ -1,8 +1,11 @@
 """Spectral representations."""
-from . import sat
+from . import sat, soundsig
 from .sat import sat_multitaper
+from .soundsig import soundsig_spectro
 
 __all__ = [
-    "sat_multitaper"
-    "sat"
+    "sat_multitaper",
+    "sat",
+    "soundsig",
+    "soundsig_spectro",
 ]
