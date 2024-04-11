22fe227e6ea2b37f6dd7eca171cff1a81685f073
David Nicholson <nickledave@users.noreply.github.com>
2024-04-11 12:41:33 -0400

    Add 'soundsig-spectro' as 'method' to vocalpy.spectrogram function

--- src/vocalpy/spectrogram.py
+++ src/vocalpy/spectrogram.py
@@ -1,21 +1,28 @@
 """Convenience function that generates a spectrogram."""
 from __future__ import annotations
 
+from typing import Mapping
+
 import librosa
 import numpy as np
 
 from . import spectral
 from ._spectrogram.data_type import Spectrogram
+from .params import Params
 from .sound import Sound
 
 
 METHODS = [
     'librosa-db',
     'sat-multitaper',
+    'soundsig-spectro',
 ]
 
-# TODO: add 'SAT' method and 'biosound' method
-def spectrogram(sound: Sound, n_fft: int = 512, hop_length: int = 64, method="librosa-db", **kwargs) -> Spectrogram:
+
+def spectrogram(
+        sound: Sound, n_fft: int = 512, hop_length: int = 64, method="librosa-db",
+        params: Mapping | Params | None = None
+) -> Spectrogram:
     """Get a spectrogram from audio.
 
     This is a convenience function that takes an instance of :class:`vocalpy.Sound`
@@ -26,10 +33,14 @@ def spectrogram(sound: Sound, n_fft: int = 512, hop_length: int = 64, method="li
     Methods
     =======
     * `'librosa-db`': dB-scaled spectrogram
-        Equivalent to calling ``S = librosa.STFT(sound.data)``
-        and then ``S = librosa.amplitude_to_db(np.abs(S))``.
+      Equivalent to calling ``S = librosa.STFT(sound.data)``
+      and then ``S = librosa.amplitude_to_db(np.abs(S))``.
     * ``'sat-multitaper``: multi-taper spectrogram computed the same way
       that the Sound Analysis Toolbox for Matlab (SAT) does.
+    * ``'soundsig-spectro``: dB-scaled spectrogram
+      computed with Gaussian window; replicates the result of
+      the method ``soundsig.BioSound.spectroCalc`` in the
+      ``soundsig`` package.
 
     Parameters
     ----------
@@ -46,6 +57,9 @@ def spectrogram(sound: Sound, n_fft: int = 512, hop_length: int = 64, method="li
     method : str
         Name  of method.
         Default is `'librosa-db'`.
+    params : vocalpy.Params, mapping, None
+        A dictionary-like mapping from function parameter names
+        to argument values.
 
     Returns
     -------
@@ -67,14 +81,12 @@ def spectrogram(sound: Sound, n_fft: int = 512, hop_length: int = 64, method="li
         S = librosa.amplitude_to_db(np.abs(S))
         t = librosa.frames_to_time(frames=np.arange(S.shape[-1]), sr=sound.samplerate, hop_length=hop_length)
         f = librosa.fft_frequencies(sr=sound.samplerate, n_fft=n_fft)
-        return Spectrogram(
-            data=S,
-            frequencies=f,
-            times=t,
-            audio_path=sound.path,
-        )
+        spect = Spectrogram(data=S, frequencies=f, times=t, audio_path=sound.path)
     elif method == 'sat-multitaper':
         spect: Spectrogram = spectral.sat_multitaper(sound, n_fft, hop_length)
-        return spect
+    elif method == 'soundsig-spectro':
+        spect: Spectrogram = spectral.soundsig_spectro(sound, n_fft, hop_length, **params)
     else:
         raise ValueError(f"Unknown method: {method}")
+
+    return spect
