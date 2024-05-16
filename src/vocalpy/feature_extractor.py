5fa2880854a0dfcbb75176582709ad61684e65b2
David Nicholson <nickledave@users.noreply.github.com>
2024-05-15 23:37:02 -0400

    fixup src/vocalpy/feature_extractor.py

--- src/vocalpy/feature_extractor.py
+++ src/vocalpy/feature_extractor.py
@@ -8,14 +8,9 @@ from typing import TYPE_CHECKING, Mapping, Union
 import dask
 import dask.diagnostics
 
-from . import Features
 
 if TYPE_CHECKING:
-    import Params
-    import Segment
-    import Segments
-    import Sound
-
+    from . import Features, Params, Segment, Segments, Sound
     FeatureSource = Union[Sound, list[Sound], Segment, list[Segment], Segments]
 
 
@@ -65,9 +60,9 @@ class FeatureExtractor:
         self.params = params
 
     def extract(self, source: FeatureSource, parallelize: bool = True) -> Features | list[Features]:
-        from . import Segment, Sound
+        from . import Segment, Segments, Sound, Features
 
-        if not isinstance(source, (list, Segments)):
+        if not isinstance(source, (list, Segment, Segments, Sound)):
             raise TypeError(
                 "`source` to extract features from must be an instance of a `Sound`, a `Segment`, "
                 f"a `list` of `Sound` or `Segment` instances, or a `Segments` instance, "
@@ -78,16 +73,24 @@ class FeatureExtractor:
             if not (
                 all([isinstance(source_, Segment) for source_ in source])
                 or all([isinstance(source_, Segments) for source_ in source])
+                or all([isinstance(source_, Sound) for source_ in source])
             ):
                 types = set(type(el) for el in source)
                 raise TypeError(
-                    "A `list` passed to `FeatureExtract.extract` must be either all `Segment` instances "
-                    f"or all `Segments` instances, but found the following types: {types}"
+                    "A `list` passed to `FeatureExtract.extract` must be either all `Segment` instances, "
+                    f"all `Segments` instances, or all `Sound` instances, but found the following types: {types}"
                 )
 
         # define nested function so vars are in scope and ``dask`` can call it
         def _to_features(source_: FeatureSource) -> Features:
-            features: Features = self.callback(source_, **self.params)
+            if isinstance(source_, Segment):
+                features = Features(
+                    data=self.callback(source_.sound, **self.params)
+                )
+            elif isinstance(source_, Sound):
+                features = Features(
+                    self.callback(source_, **self.params)
+                )
             return features
 
         if isinstance(source, (Sound, Segment)):
@@ -95,8 +98,8 @@ class FeatureExtractor:
 
         elif (
             isinstance(source, Segments)
-            or (isinstance(source, list) and all([isinstance(source, Sound)]))
-            or (isinstance(source, list) and all([isinstance(source, Segment)]))
+            or (isinstance(source, list) and all([isinstance(source_, Sound) for source_ in source]))
+            or (isinstance(source, list) and all([isinstance(source_, Segment) for source_ in source]))
         ):
             features = []
             for source_ in source:
