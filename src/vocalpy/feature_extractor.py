c85ecccf470f6212a94976c9d20c0049b029b94d
David Nicholson <nickledave@users.noreply.github.com>
2024-05-23 22:48:00 -0400

    Rewrite FeatureExtractor to only take Sound or list of Sounds, fix #163

--- src/vocalpy/feature_extractor.py
+++ src/vocalpy/feature_extractor.py
@@ -3,15 +3,13 @@ from __future__ import annotations
 
 import collections.abc
 import inspect
-from typing import TYPE_CHECKING, Mapping, Union
+from typing import TYPE_CHECKING, Mapping
 
 import dask
 import dask.diagnostics
 
 if TYPE_CHECKING:
-    from . import Features, Params, Segment, Segments, Sound
-
-    FeatureSource = Union[Sound, list[Sound], Segment, list[Segment], Segments]
+    from . import Features, Params, Sound
 
 
 class FeatureExtractor:
@@ -62,50 +60,38 @@ class FeatureExtractor:
     def __repr__(self):
         return f"FeatureExtractor(callback={self.callback.__qualname__}, params={self.params})"
 
-    def extract(self, source: FeatureSource, parallelize: bool = True) -> Features | list[Features]:
-        from . import Features, Segment, Segments, Sound
+    def extract(self, sound: Sound | list[Sound], parallelize: bool = True) -> Features | list[Features]:
+        from . import Features, Sound
 
-        if not isinstance(source, (list, Segment, Segments, Sound)):
+        if not isinstance(sound, (list, Sound)):
             raise TypeError(
-                "`source` to extract features from must be an instance of a `Sound`, a `Segment`, "
-                f"a `list` of `Sound` or `Segment` instances, or a `Segments` instance, "
-                f"but type was: {type(source)}"
+                "`sound` must be an instance of a `Sound` "
+                f"or a `list` of `Sound` instances, "
+                f"but type was: {type(sound)}"
             )
 
-        if isinstance(source, list):
-            if not (
-                all([isinstance(source_, Segment) for source_ in source])
-                or all([isinstance(source_, Segments) for source_ in source])
-                or all([isinstance(source_, Sound) for source_ in source])
-            ):
-                types = set(type(el) for el in source)
+        if isinstance(sound, list):
+            if not all([isinstance(sound_, Sound) for sound_ in sound]):
+                types = set(type(el) for el in sound)
                 raise TypeError(
-                    "A `list` passed to `FeatureExtract.extract` must be either all `Segment` instances, "
-                    f"all `Segments` instances, or all `Sound` instances, but found the following types: {types}"
+                    "A `list` passed to `FeatureExtract.extract` must be all `Sound` instances, "
+                    f"but found the following types: {types}"
                 )
 
         # define nested function so vars are in scope and ``dask`` can call it
-        def _to_features(source_: FeatureSource) -> Features:
-            if isinstance(source_, Segment):
-                features = Features(data=self.callback(source_.sound, **self.params))
-            elif isinstance(source_, Sound):
-                features = Features(self.callback(source_, **self.params))
-            return features
+        def _to_features(sound_: Sound) -> Features:
+            return self.callback(sound_, **self.params)
 
-        if isinstance(source, (Sound, Segment)):
-            return _to_features(source)
+        if isinstance(sound, Sound):
+            return _to_features(sound)
 
-        elif (
-            isinstance(source, Segments)
-            or (isinstance(source, list) and all([isinstance(source_, Sound) for source_ in source]))
-            or (isinstance(source, list) and all([isinstance(source_, Segment) for source_ in source]))
-        ):
+        elif isinstance(sound, list) and all([isinstance(sound_, Sound) for sound_ in sound]):
             features = []
-            for source_ in source:
+            for sound_ in sound:
                 if parallelize:
-                    features.append(dask.delayed(_to_features)(source_))
+                    features.append(dask.delayed(_to_features)(sound_))
                 else:
-                    features.append(_to_features(source_))
+                    features.append(_to_features(sound_))
 
             if parallelize:
                 graph = dask.delayed()(features)
@@ -113,20 +99,3 @@ class FeatureExtractor:
                     return graph.compute()
             else:
                 return features
-
-        elif isinstance(source, list) and all([isinstance(source, Segments)]):
-            features_list = []
-            for segments in source:
-                features = []
-                for segment in segments:
-                    if parallelize:
-                        features.append(dask.delayed(_to_features)(segment))
-                    else:
-                        features.append(_to_features(segment))
-
-            if parallelize:
-                graph = dask.delayed()(features_list)
-                with dask.diagnostics.ProgressBar():
-                    return graph.compute()
-            else:
-                return features_list
