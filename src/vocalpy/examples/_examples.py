f3d40e34a350c916fa213b6c3e0cdcb1f71d4325
David Nicholson <NickleDave@users.noreply.github.com>
2024-10-13 21:07:45 -0400

    Refactor code for example data to be like crowsetta.examples, fix #185

--- src/vocalpy/examples/_examples.py
+++ src/vocalpy/examples/_examples.py
@@ -3,82 +3,36 @@
 from __future__ import annotations
 
 import importlib.resources
+import json
 import os
 import pathlib
 import shutil
 from dataclasses import dataclass
 from enum import Enum
-from typing import Literal
+from typing import TYPE_CHECKING, Union
 
+from attr import define
 import pooch
 import requests.exceptions
 
 from .example_data import ExampleData
 
-# ---- define this at the top since we use it for ExampleMeta
-ExampleTypes = Enum("Exampletypes", "Sound Spectrogram Annotation ExampleData")
 
+if TYPE_CHECKING:
+    import vocalpy
 
-# ---- define this before `makefuncs` that return ExampleData,
-# since it is the return type
-@dataclass
-class ExampleMeta:
-    """Dataclass that represents example data,
-    including metadata needed to handle them,
-    and metadata specific to the dataset
-    that is displayed by VocalPy.
-
-    Attributes
-    ----------
-    name : str
-        Name of example. Displayed by :func:`vocalpy.examples.show`.
-        By default, this should be the filename.
-        If the filename is not easy enough to type,
-        this can be a simple human-readable name,
-        and a separate ``fname`` attribute can specify the filename.
-    metadata : str
-        Metadata for example, that links to source for data.
-        Displayed by :func:`vocalpy.examples.show`.
-    type: ExampleType
-        Type of data. Default is :class:`vocalpy.Sound`.
-    fname: str, optional
-        Filename. By default, the name should be the filename,
-        but if a filename is too verbose (e.g. a .tar.gz file),
-        this attribute allows ``name`` to be something human-readable.
-        Default is None.
-    requires_download: bool
-        If True, this example requires download, and we use
-        :module:`pooch` to "fetch" it.
-    annot_format: str, optional
-        For examples that have annotations,
-        the annotation format.
-        String that is recognized by :module:`crowsetta`
-        as a valid annotation format.
-    makefunc: callable
-        For examples that return :class:`ExampleData`,
-        a callable function that returns an :class`ExampleData`
-        instance with the example data.
-    """
-
-    name: str
-    metadata: str
-    type: Literal[ExampleTypes.Sound, ExampleTypes.Spectrogram, ExampleTypes.Annotation, ExampleTypes.ExampleData] = (
-        ExampleTypes.Sound
-    )
-    fname: str | None = None
-    # next line: lambda returns a new dict that maps 'sound' to 'wav' extension
-    requires_download: bool = False
-    annot_format: str | None = None
-    makefunc: callable | None = None
-
-    def __post_init__(self):
-        if not any([self.type is example_type for example_type in ExampleTypes]):
-            raise ValueError(f"example type '{self.type}' is not one of the ExampleTypes: {ExampleTypes}")
+    ExampleType = Union[
+        pathlib.Path,
+        vocalpy.Sound,
+        vocalpy.Spectrogram,
+        vocalpy.Annotation,
+        ExampleData,
+    ]
 
 
 # ---- all `makefunc`s that return ExampleData for larger example datasets go here
 def bfsongrepo_makefunc(
-    path: pathlib.Path | list[pathlib.Path], metadata: ExampleMeta, return_path: bool = False
+    path: pathlib.Path | list[pathlib.Path], return_path: bool = False, annot_format: str = "simple-seq"
 ) -> ExampleData:
     import vocalpy  # avoid circular import
 
@@ -89,12 +43,12 @@ def bfsongrepo_makefunc(
     else:
         return ExampleData(
             sound=[vocalpy.Sound.read(wav_path) for wav_path in wav_paths],
-            annotation=[vocalpy.Annotation.read(csv_path, format=metadata.annot_format) for csv_path in csv_paths],
+            annotation=[vocalpy.Annotation.read(csv_path, format=annot_format) for csv_path in csv_paths],
         )
 
 
 def jourjine_et_al_2023_makefunc(
-    path: pathlib.Path | list[pathlib.Path], metadata: ExampleMeta, return_path: bool = False
+    path: pathlib.Path | list[pathlib.Path], return_path: bool = False
 ) -> ExampleData:
     """Make ``'jourjine-et-al-2023'`` example data"""
     import vocalpy  # avoid circular import
@@ -115,107 +69,203 @@ def jourjine_et_al_2023_makefunc(
             ),
         )
 
-
-# ---- now that we've declared all the `makefunc`s we can actually describe all the example data
-EXAMPLE_METADATA = [
-    ExampleMeta(
-        name="bells.wav",
-        metadata="Zebra finch song from Sound Analysis Pro website: http://soundanalysispro.com/",
-    ),
-    ExampleMeta(
-        name="flashcam.wav",
-        metadata="Zebra finch song from Sound Analysis Pro website: http://soundanalysispro.com/",
-    ),
-    ExampleMeta(
-        name="samba.wav",
-        metadata="Zebra finch song from Sound Analysis Pro website: http://soundanalysispro.com/",
-    ),
-    ExampleMeta(
-        name="simple.wav",
-        metadata="Zebra finch song from Sound Analysis Pro website: http://soundanalysispro.com/",
-    ),
-    ExampleMeta(
-        name="BM003.wav",
-        metadata="""Mouse ultrasonic vocalization from:
-Goffinet, J., Brudner, S., Mooney, R., & Pearson, J. (2021).
-Data from: Low-dimensional learned feature spaces quantify individual and group differences
-in vocal repertoires. Duke Research Data Repository. https://doi.org/10.7924/r4gq6zn8w.
-Adapted under Creative Commons License 1.0: https://creativecommons.org/publicdomain/zero/1.0/.
-File name in dataset: BM003_day9_air_20s_sparse_chunk007_0297.wav""",
-    ),
-    ExampleMeta(
-        name="bl26lb16.wav",
-        metadata="""Bengalese finch song from:
-Bengalese Finch Song Repository.
-Nicholson, David; Queen, Jonah E.; J. Sober, Samuel (2017). Bengalese Finch song repository. figshare.
-Dataset. https://doi.org/10.6084/m9.figshare.4805749.v9
-https://nickledave.github.io/bfsongrepo
-
-In the source dataset, this audio file is named
-``bl2616lb16/041912/bl26lb16_190412_0834.20350.wav``.
-        """,
-    ),
-    ExampleMeta(
-        name="deermouse-go.wav",
-        metadata="""Deer mouse pup calls from:
-Jourjine, Nicholas et al. (2023). Data from:
-Two pup vocalization types are genetically and functionally separable in deer mice [Dataset].
-Dryad. https://doi.org/10.5061/dryad.g79cnp5ts
-
-This is a short clip from the start of the file named
-``GO_24860x23748_ltr2_pup3_ch4_4800_m_337_295_fr1_p9_2021-10-02_12-35-01.wav``
-in the source dataset.
-""",
-    ),
-    ExampleMeta(
-        name="fruitfly-song-multichannel.wav",
-        metadata="""courtship song (pulse and sine) of male Drosophila melanogaster. Data from:
-Clemens, Jan, 2021, "Drosophila melanogaster - train and test data (multi channel)", https://doi.org/10.25625/8KAKHJ, GRO.data, V1
-Contains recordings (on 9 microphone channels) and manual annotations of the courtship song (pulse and sine) of male Drosophila melanogaster.
-
-The recordings were previously unpublished and were first used in:
-Clemens J, Coen P, Roemschied FA, Pereira TD, Mazumder D, Aldarondo DE, Pacheco DA, Murthy M. 2018.
-Discovery of a New Song Mode in Drosophila Reveals Hidden Structure in the Sensory and Neural Drivers of Behavior.
-Current biology 28:2400–2412.e6.
-
-This is a short clip from the file named
-``160420_1746_manual.wav`` in the source dataset,
-that was clipped using the tsv annotations created with the DAS gui.
-To minimize file size, only three of the channels from the original nine are kept.
-""",
-    ),
-    ExampleMeta(
-        name="bfsongrepo",
-        metadata="""Sample of song from Bengalese Finch Song Repository.
-Nicholson, David; Queen, Jonah E.; J. Sober, Samuel (2017). Bengalese Finch song repository. figshare.
-Dataset. https://doi.org/10.6084/m9.figshare.4805749.v9
-https://nickledave.github.io/bfsongrepo
-Files are approximately 20 songs from bird with ID "gy6or6", from the day "032312"
-""",
-        type=ExampleTypes.ExampleData,
-        fname="bfsongrepo.tar.gz",
-        requires_download=True,
-        makefunc=bfsongrepo_makefunc,
-        annot_format="simple-seq",
-    ),
-    ExampleMeta(
-        name="jourjine-et-al-2023",
-        metadata="""Sample of deer mouse vocalizations from:
-Jourjine, Nicholas et al. (2023). Data from:
-Two pup vocalization types are genetically and functionally separable in deer mice [Dataset].
-Dryad. https://doi.org/10.5061/dryad.g79cnp5ts
-""",
-        type=ExampleTypes.ExampleData,
-        fname="jourjine-et-al-2023.tar.gz",
-        requires_download=True,
-        makefunc=jourjine_et_al_2023_makefunc,
-        annot_format="simple-seq",
-    ),
+MAKEFUNCS = [
+    bfsongrepo_makefunc,
+    jourjine_et_al_2023_makefunc,
 ]
 
+MAKEFUNCS_MAP = {
+    makefunc.__name__: makefunc
+    for makefunc in MAKEFUNCS
+}
 
-REGISTRY = {example.name: example for example in EXAMPLE_METADATA}
+ExampleTypes = Enum("Exampletypes", "Sound Spectrogram Annotation ExampleData")
 
+
+@define
+class Example:
+    """Class that represents example data.
+
+    Attributes
+    ----------
+    name : str
+        Human-readable name of example data
+    description : str
+        Description of example data, 
+        including any relevant citations.
+    type : ExampleTypes
+        Type of data.
+        A :class:`Enum` member that is used
+        in the :meth:`Example.load` method 
+        to determine how to load the data.
+    requires_download: bool
+        If ``True``, this example data requires a download.
+        The :meth:`Example.load` method will call :mod:`pooch`.
+    path : pathlib.Path, optional
+        For examples that are a single file,
+        this is the path to the file.
+    makefunc : callable, optional
+        For examples that are multiple files,
+        this is a function that returns an 
+        :class:`ExampleData` instance 
+        with attributes containing the 
+        multiple files.
+    makefunc_kwargs : dict, optional
+        A :class:`dict` of keyword arguments
+        to pass into :attr:`Exanple.makefunc`.
+
+    Notes
+    -----
+    This dataclass is used to load metadata from
+    `vocalpy/examples/example-metadata.json`.
+    """
+    name: str
+    description: str
+    type: ExampleTypes
+    requires_download: bool
+    path: pathlib.Path | None = None
+    makefunc: callable | None = None
+    makefunc_kwargs: dict | None = None
+
+    @classmethod
+    def from_metadata(
+        cls, 
+        description_filename: str,
+        example_type: str,
+        name: str | None = None,
+        filename: str | None = None,
+        requires_download: bool = False,
+        makefunc_name: str | None = None,
+        makefunc_kwargs: dict | None = None,
+    ):
+        """Create a :class:`Example` instance from metadata.
+
+        Parameters
+        ----------
+        description_filename: str
+            Name of text file that contains description of
+            examnple data, including any relevant citations.
+        example_type: str
+            String name of example type,
+            that should match one member of the :class:`Enum`
+            ``ExampleTypes``.
+            The :meth:`Example.load` method uses this 
+            to determine how to load the example.
+        name: str, optional
+            A human-readable name for the example.
+            If None, defaults to filename.
+        filename : str
+            For examples that are a single file,
+            the name of the file.
+        makefunc_name : string, optional
+            For examples that are multiple files,
+            the name of the function that 
+            returns an instance of :class:`ExampleData`
+            with attributes that contain data loaded 
+            from the files.
+        makefunc_kwargs : dict, optional
+            A :class:`dict` of keyword arguments to 
+            pass into the ``makefunc``.
+            Optional, default is None.
+
+        Returns
+        -------
+        example : Example
+            Instance of :class:`Example` dataclass
+        """
+        if filename is None and name is None:
+                raise ValueError(
+                    "`name` and `filename` for example can't both be None"
+                )
+
+        if name is None:
+            name = filename
+
+        description_path = importlib.resources.files("vocalpy.examples").joinpath(description_filename)
+        description = description_path.read_text()
+
+        type_ = ExampleTypes[example_type]
+
+        if filename:
+            path = importlib.resources.files("vocalpy.examples").joinpath(filename)
+
+        if makefunc_name is not None:
+            makefunc = MAKEFUNCS_MAP[makefunc_name]
+        else:
+            makefunc = None
+
+        return cls(
+            name,
+            description,
+            type_,
+            requires_download,
+            path,
+            makefunc,
+            makefunc_kwargs
+        )
+
+    def __attrs_post_init__(self):
+        if self.name is None:
+            raise ValueError(
+                "`name` can't be None"
+            )
+
+        if not any([self.type is example_type for example_type in ExampleTypes]):
+            raise ValueError(f"example type '{self.type}' is not one of the ExampleTypes: {ExampleTypes}")
+
+    def load(self, return_path: bool):
+        import vocalpy
+
+        if self.requires_download:
+            try:
+                POOCH.load_registry_from_doi()
+            except requests.exceptions.ConnectionError as e:
+                raise ConnectionError(
+                    "Unable to connect to registry to download example dataset. "
+                    "This may be due to an issue with an internet connection."
+                ) from e
+            if self.fname.endswith(".tar.gz"):
+                path = POOCH.fetch(self.fname, processor=pooch.Untar())
+            else:
+                path = POOCH.fetch(self.fname)
+            if isinstance(path, list):
+                path = [pathlib.Path(path_) for path_ in path]
+            else:
+                path = pathlib.Path(path)
+        else:
+            path = self.path
+
+        if isinstance(path, list):
+            # enforce consisting sorting across platforms
+            path = sorted(path)
+
+        if return_path:
+            if self.type == ExampleTypes.ExampleData:
+                return self.makefunc(path, metadata=self, return_path=return_path)
+            else:
+                return path
+        else:
+            if self.type == ExampleTypes.Sound:
+                return vocalpy.Sound.read(path)
+            elif self.type == ExampleTypes.Spectrogram:
+                return vocalpy.Spectrogram.read(path)
+            elif self.type == ExampleTypes.Annotation:
+                return vocalpy.Annotation.read(path, format=self.annot_format)
+            elif self.type == ExampleTypes.ExampleData:
+                return self.makefunc(path, metadata=self, return_path=return_path)
+
+
+EXAMPLE_METADATA_JSON_PATH = pathlib.Path(
+    importlib.resources.files("vocalpy.examples").joinpath("example-metadata.json")
+)
+with EXAMPLE_METADATA_JSON_PATH.open("r") as fp:
+    ALL_EXAMPLE_METADATA = json.load(fp)
+
+EXAMPLES = [Example.from_metadata(**example_metadata) for example_metadata in ALL_EXAMPLE_METADATA]
+
+REGISTRY = {example_.name: example_ for example_ in EXAMPLES}
+
+# ---- pooch set-up ------------------------------
 VOCALPY_DATA_DIR = "VOCALPY_DATA_DIR"
 
 ZENODO_DATASET_BASE_URL = "doi:10.5281/zenodo.10685639"
@@ -299,42 +349,8 @@ def example(name: str, return_path: bool = False) -> ExampleType:
         )
     import vocalpy  # avoid circular import
 
-    example_: ExampleMeta = REGISTRY[name]
-    if example_.requires_download:
-        try:
-            POOCH.load_registry_from_doi()
-        except requests.exceptions.ConnectionError as e:
-            raise ConnectionError(
-                "Unable to connect to registry to download example dataset. "
-                "This may be due to an issue with an internet connection."
-            ) from e
-        if example_.fname.endswith(".tar.gz"):
-            path = POOCH.fetch(example_.fname, processor=pooch.Untar())
-        else:
-            path = POOCH.fetch(example_.fname)
-        if isinstance(path, list):
-            path = [pathlib.Path(path_) for path_ in path]
-        else:
-            path = pathlib.Path(path)
-    else:
-        path = pathlib.Path(importlib.resources.files("vocalpy.examples").joinpath(name))
-    if isinstance(path, list):
-        # enforce consisting sorting across platforms
-        path = sorted(path)
-    if return_path:
-        if example_.type == ExampleTypes.ExampleData:
-            return example_.makefunc(path, metadata=example_, return_path=return_path)
-        else:
-            return path
-    else:
-        if example_.type == ExampleTypes.Sound:
-            return vocalpy.Sound.read(path)
-        elif example_.type == ExampleTypes.Spectrogram:
-            return vocalpy.Spectrogram.read(path)
-        elif example_.type == ExampleTypes.Annotation:
-            return vocalpy.Annotation.read(path, format=example_.annot_format)
-        elif example_.type == ExampleTypes.ExampleData:
-            return example_.makefunc(path, metadata=example_, return_path=return_path)
+    example_: Example = REGISTRY[name]
+    return example_.load(return_path=return_path)
 
 
 def show() -> None:
@@ -355,7 +371,7 @@ def show() -> None:
     -----
     To retrieve an example, call :func:`vocalpy.example`.
     """
-    print("Examples built into VocalPy")
+    print("VocalPy example data")
     print("=" * 72)
-    for example_meta in EXAMPLE_METADATA:
-        print(f"name: {example_meta.name}\n" "metadata:\n" f"{example_meta.metadata}\n")
+    for example in EXAMPLES:
+        print(f"name: {example.name}\n" "description:\n" f"{example.description}\n")
