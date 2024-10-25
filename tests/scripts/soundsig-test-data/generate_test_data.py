6d6ac8df562ddd444624c4138ffbccd79abbf5a7
David Nicholson <NickleDave@users.noreply.github.com>
2024-10-25 14:25:36 -0400

    Add print statement to tests/scripts/soundsig-test-data/generate_test_data.py

--- tests/scripts/soundsig-test-data/generate_test_data.py
+++ tests/scripts/soundsig-test-data/generate_test_data.py
@@ -4,3 +4,5 @@ from soundsig.sound import BioSound
 
 TEST_DATA_ROOT = pathlib.Path(__file__).parent / ".." / ".." / "data-for-tests"
 assert TEST_DATA_ROOT.exists()
+
+print("BioSound is:", BioSound)
