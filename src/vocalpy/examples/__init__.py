634b9a5ec5ae3b9dc8929b642727dc57d2296137
David Nicholson <NickleDave@users.noreply.github.com>
2024-11-22 13:39:59 -0500

    Import ExampleData in vocalpy/examples/__init__.py

--- src/vocalpy/examples/__init__.py
+++ src/vocalpy/examples/__init__.py
@@ -1,10 +1,12 @@
 """Example data built into VocalPy."""
 
 from ._examples import clear_cache, example, get_cache_dir, show
+from .example_data import ExampleData
 
 __all__ = [
     "clear_cache",
     "get_cache_dir",
     "example",
+    "ExampleData",
     "show",
 ]
