bca50ad661b5f93b1927f14917734becb32c064d
David Nicholson <NickleDave@users.noreply.github.com>
2024-11-27 12:48:57 -0500

    Remove autosummary directive from vocalpy/plot/__init__.py, add annot and spect modules to __all__ so they get stubs generated by autosummary in api/index.rst

--- src/vocalpy/plot/__init__.py
+++ src/vocalpy/plot/__init__.py
@@ -1,18 +1,13 @@
-"""Module for plotting and visualizations.
+"""Module for plotting and visualizations."""
 
-.. autosummary::
-   :toctree: generated/
-
-   annotated_spectrogram
-   annotation
-   spectrogram
-"""
-
+from . import annot, spect
 from .annot import annotation
 from .spect import annotated_spectrogram, spectrogram
 
 __all__ = [
+    "annot",
     "annotated_spectrogram",
     "annotation",
+    "spect",
     "spectrogram",
 ]
